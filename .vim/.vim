8d4e9d2bf0c250d63e2245b0a2508a97e8741ae5
lijunjie <lijunjie01@souche.com>
2020-08-13 17:48:34 +0800

    1.tmux 2.vim-exchange

---
+++ .vim/.vim
@@ -0,0 +1,1 @@
+/Users/dasouche/.dotfiles/.vim
--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 ln -s ~/.dotfiles/.vim ~/.vim
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 cp ~/.dotfiles/zsh/lu5je0.zsh-theme ~/.oh-my-zsh/themes
