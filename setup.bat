2c10b317217a444d7b0ffc15705b5396296ecc34
lu5je0 <lu5je0@gmail.com>
2020-11-14 19:10:31 +0800

    ssh config for win

--- setup.bat
+++ setup.bat
@@ -2,3 +2,4 @@ c:
 cd %HOMEPATH%
 mklink /J %HOMEPATH%\vimfiles %HOMEPATH%\.dotfiles\.vim
 mklink /H %HOMEPATH%\.ideavimrc %HOMEPATH%\.dotfiles\.ideavimrc
+mklink /H %HOMEPATH%\.ssh\config %HOMEPATH%\.dotfiles\.ssh\config_win
