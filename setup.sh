3a0845383da56a226eba4cbbfcfdf0bb2f8d5c58
lu5je0 <lu5je0@gmail.com>
2020-12-27 14:03:59 +0800

    rm .vim/.vim

--- setup.sh
+++ setup.sh
@@ -14,3 +14,4 @@ ln -s ~/.dotfiles/.vim/coc-settings.json ~/.config/nvim/coc-settings.json
 cp ~/.dotfiles/zsh/lu5je0.zsh-theme ~/.oh-my-zsh/themes
 
 ln -s ~/.dotfiles/.ssh/config ~/.ssh/config
+rm ~/.dotfiles/.vim/.vim
