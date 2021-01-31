313bd0068b250a26e92c36e6a87cb560ff86619c
lu5je0 <lu5je0@gmail.com>
2021-01-31 19:48:51 +0800

    setup.sh

--- setup.sh
+++ setup.sh
@@ -20,3 +20,4 @@ rm ~/.dotfiles/.vim/.vim
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/paulirish/git-open.git ~/.oh-my-zsh/custom/plugins/git-open
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
