26232efaa050f7febc78c64bc345b1dde2cc7f3c
lijunjie <lijunjie01@souche.com>
2021-02-03 17:32:35 +0800

    隐藏macos dock栏小火箭

--- .vim/python/im.py
+++ .vim/python/im.py
@@ -3,6 +3,14 @@ import ctypes
 import time
 import os
 
+try:
+    import AppKit
+    # 隐藏macos dock栏小火箭
+    info = AppKit.NSBundle.mainBundle().infoDictionary()
+    info["LSBackgroundOnly"] = "1"
+except ImportError:
+    print("隐藏macos dock栏小火箭,需要pip3 install -U PyObjC")
+
 lib = ctypes.CDLL(os.environ['HOME'] + "/.dotfiles/lib/libinput-source-switcher.dylib")
 lib.getCurrentInputSourceID.restype = c_char_p
 
