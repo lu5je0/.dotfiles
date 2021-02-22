d2007a8273c23be6840fb2b0856b753b08ad6f8e
lu5je0 <lu5je0@gmail.com>
2021-02-22 21:52:54 +0800

    nvim setup.bat

--- setup.bat
+++ setup.bat
@@ -1,5 +1,8 @@
 c:
 cd %HOMEPATH%
 mklink /J %HOMEPATH%\vimfiles %HOMEPATH%\.dotfiles\.vim
+mklink /J %HOMEPATH%\AppData\Local\nvim %HOMEPATH%\.dotfiles\.vim
+mklink %HOMEPATH%\AppData\Local\nvim\init.vim %HOMEPATH%\.dotfiles\.vim\vimrc
 mklink %HOMEPATH%\.ideavimrc %HOMEPATH%\.dotfiles\.ideavimrc
+mklink %HOMEPATH%\AppData\Local\nvim\init.vim %HOMEPATH%\.dotfiles\.vim\vimrc
 mklink %HOMEPATH%\.ssh\config %HOMEPATH%\.dotfiles\.ssh\config_win
