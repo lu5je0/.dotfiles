b415b5a409d855ae9c5abc630e09d4e9575f1b26
lu5je0 <lu5je0@gmail.com>
2021-06-26 19:58:40 +0800

    zsh

---
+++ zsh/platform-alias.sh
@@ -0,0 +1,20 @@
+# mac
+if [ "$(uname)" = "Darwin" ]; then
+    PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+    alias ls='ls -F --show-control-chars --color=auto'
+    eval `gdircolors -b $HOME/.dir_colors`
+    export JAVA_HOME_8=/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home
+    export JAVA_HOME_11=/Library/Java/JavaVirtualMachines/openjdk-11.0.2.jdk/Contents/Home
+    alias jdk8='export JAVA_HOME=$JAVA_HOME_8'
+    alias e='open'
+    alias jdk11='export JAVA_HOME=$JAVA_HOME_11'
+    alias iterm='open -a iTerm .'
+    export JAVA_HOME=$JAVA_HOME_11
+    ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#555555"
+elif [ $(uname -r | sed -n 's/.*\( *Microsoft *\).*/\1/ip') ]; then
+    alias e='explorer.exe'
+    alias cmd='/mnt/c/Windows/System32/cmd.exe /c'
+    clippaste () {
+        powershell.exe -noprofile -command Get-Clipboard | tr -d '\r'
+    }
+fi
