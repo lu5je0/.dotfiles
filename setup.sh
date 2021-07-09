0239d7945dd8d5dee898082ded030cf494243f29
lijunjie <lijunjie01@souche.com>
2021-07-09 13:57:27 +0800

    remove cheat

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 ln -s ~/.dotfiles/.vim ~/.vim
-ln -s ~/.dotfiles/.cheat ~/.cheat
 ln -s ~/.dotfiles/.ideavimrc ~/.ideavimrc
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
