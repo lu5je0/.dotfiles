20e49441aacd2a7edd4cd76b18682746cd27b71e
lijunjie <lijunjie01@souche.com>
2021-07-19 10:09:44 +0800

    优化输入法切换

--- .vim/python/im.py
+++ .vim/python/im.py
@@ -1,15 +1,40 @@
-import os
-from AppKit import NSTextInputContext,NSTextView,NSBundle
-from concurrent.futures import ThreadPoolExecutor
+import Foundation
+from AppKit import NSObject, NSTextInputContext , NSTextView, NSTextInputContextKeyboardSelectionDidChangeNotification, NSBundle
+import multiprocessing
+import ctypes
+from PyObjCTools import AppHelper
+import objc
+
+class Observer(NSObject):
+
+    def initWithValue_(self, txtObj):
+        self = objc.super(Observer, self).init()
+        self.text_input_context = txtObj
+        return self
+
+    def bundle_(self, aNotification):
+        self.ime.value = str(self.text_input_context.selectedKeyboardInputSource())
+
+    @staticmethod
+    def watch(ime):
+        txtObj = NSTextInputContext.alloc().initWithClient_( NSTextView.new() )
+
+        obs = Observer.new().initWithValue_(txtObj)
+        obs.ime = ime
+
+        Foundation.NSNotificationCenter.defaultCenter().addObserver_selector_name_object_(obs, 'bundle:', NSTextInputContextKeyboardSelectionDidChangeNotification, None)
+        AppHelper.runConsoleEventLoop()
 
 class ImSwitcher():
     mac_ime = 'com.apple.keylayout.ABC'
 
     def __init__(self) -> None:
         self.text_input_context = NSTextInputContext.alloc().initWithClient_(NSTextView.new())
-        self.last_ime = 'com.apple.keylayout.ABC'
-        self.is_save_ime = False
-        # self.thread_pool = ThreadPoolExecutor(max_workers=1)
+        self.last_ime = ImSwitcher.mac_ime
+        self.is_save_ime = True
+        manager = multiprocessing.Manager()
+        self.cur_ime = manager.Value(ctypes.c_wchar_p, ImSwitcher.mac_ime)
+        multiprocessing.Process(target=Observer.watch, args=(self.cur_ime,)).start()
         try:
             # 隐藏macos dock栏小火箭
             info = NSBundle.mainBundle().infoDictionary()
@@ -21,11 +46,10 @@ class ImSwitcher():
         self.switch_input_source(self.last_ime)
 
     def get_cur_ime(self):
-        return os.popen('im-select').read()[0:-1]
+        return self.cur_ime.value
 
     def toggle_save_last_ime(self):
         self.is_save_ime = not self.is_save_ime
-        print("save_last_ime", self.is_save_ime)
         
     def save_last_ime(self):
         self.last_ime = self.get_cur_ime()
@@ -34,7 +58,6 @@ class ImSwitcher():
         if self.is_save_ime:
             self.save_last_ime()
         self.switch_input_source(self.mac_ime)
-        # self.thread_pool.submit(self.save_last_ime)
 
     def switch_input_source(self, input_method):
         self.text_input_context.setValue_forKey_(input_method, 'selectedKeyboardInputSource')
