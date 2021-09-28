9bf91635abf5f3f19e8af1789a688ca2f3d01600
lu5je0 <lu5je0@gmail.com>
2021-09-28 14:53:00 +0800

    nvim-lspconfig

--- .vim/lua/plugins.lua
+++ .vim/lua/plugins.lua
@@ -19,6 +19,53 @@ return require('packer').startup(function()
   }
 
   use {
+      'neovim/nvim-lspconfig',
+      config = function() require("config/lspconfig") end
+  }
+
+  use {
+      'hrsh7th/nvim-compe',
+      config = function() 
+          vim.o.completeopt = "menu,preview,noinsert"
+          -- vim.api.nvim_set_keymap("i", "<CR>", "compe#confirm({ 'keys': '<CR>', 'select': v:true })", { expr = true })
+          -- vim.api.nvim_set_keymap("i", "<tab>", "compe#confirm({ 'keys': '<CR>', 'select': v:true })", { expr = true })
+          require'compe'.setup {
+              preselect = 'always';
+              enabled = true;
+              autocomplete = true;
+              debug = false;
+              min_length = 1;
+              throttle_time = 80;
+              source_timeout = 200;
+              resolve_timeout = 800;
+              incomplete_delay = 400;
+              max_abbr_width = 100;
+              max_kind_width = 100;
+              max_menu_width = 100;
+              documentation = {
+                  border = { '', '' ,'', ' ', '', '', '', ' ' }, -- the border option is the same as `|help nvim_open_win|`
+                  winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
+                  max_width = 120,
+                  min_width = 60,
+                  max_height = math.floor(vim.o.lines * 0.3),
+                  min_height = 1,
+              };
+
+              source = {
+                  path = true;
+                  buffer = true;
+                  calc = true;
+                  nvim_lsp = true;
+                  nvim_lua = true;
+                  vsnip = true;
+                  ultisnips = true;
+                  luasnip = true;
+              };
+          }
+      end
+  }
+
+  use {
       'hrsh7th/vim-eft',
       config = function()
           vim.cmd([[
@@ -289,13 +336,13 @@ return require('packer').startup(function()
       cmd = 'MarkdownPreview'
   }
 
+  -- use {
+  --     'neoclide/coc.nvim', branch = 'release',
+  --     opt = true,
+  --     config = function() vim.cmd('runtime plug-config/coc.vim') end
+  -- }
   use {
-      'neoclide/coc.nvim', branch = 'release',
-      opt = true,
-      config = function() vim.cmd('runtime plug-config/coc.vim') end
-  }
-
-  use {'liuchengxu/vim-which-key', 
+      'liuchengxu/vim-which-key',
       config = function() vim.cmd('runtime whichkey.vim') end,
       opt = true,
       keys = {'<leader>'}
