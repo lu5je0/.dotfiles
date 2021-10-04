636874d81bd85d1858cfa2e1d5c77ba2443f7dec
lu5je0 <lu5je0@gmail.com>
2021-10-05 02:00:07 +0800

    update download-stardict.sh

--- scripts/download-stardict.sh
+++ scripts/download-stardict.sh
@@ -1,8 +1,5 @@
 wget https://github.com/skywind3000/ECDICT/releases/download/1.0.28/ecdict-sqlite-28.zip
-
-if [[ ! -d ~/.misc ]]; then
-    mkdir ~/.misc
-fi
+mkdir ~/.misc
 unzip ecdict-sqlite-28.zip
 mv stardict.db ~/.misc/
 rm ecdict-sqlite-28.zip
