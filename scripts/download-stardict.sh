8c0a51f061423b2cb617717417ae738e1394b33c
lu5je0 <lu5je0@gmail.com>
2021-10-07 19:21:46 +0800

    download-stardict.sh checks unzip and wget is existing

--- scripts/download-stardict.sh
+++ scripts/download-stardict.sh
@@ -1,3 +1,6 @@
+type unzip >/dev/null 2>&1 || { echo >&2 "unzip not installed. Aborting."; exit 1; }
+type wget >/dev/null 2>&1 || { echo >&2 "wget not installed. Aborting."; exit 1; }
+
 wget https://github.com/skywind3000/ECDICT/releases/download/1.0.28/ecdict-sqlite-28.zip
 mkdir ~/.misc
 unzip ecdict-sqlite-28.zip
