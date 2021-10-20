807163eec5ed5f91f5016eab53fe434196c0462e
lu5je0 <lu5je0@gmail.com>
2021-10-20 17:00:12 +0800

    format

--- .vim/lua/plugins.lua
+++ .vim/lua/plugins.lua
@@ -116,7 +116,7 @@ return require('packer').startup(function()
   use {
       'tpope/vim-dadbod',
       config = function ()
-          vim.g.db_ui_use_nerd_fonts=1
+          vim.g.db_ui_use_nerd_fonts = 1
           vim.g.db_ui_winwidth = 30
       end,
       opt = true,
