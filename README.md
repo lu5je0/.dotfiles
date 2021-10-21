24820053c4894e4fdebcb226aa2734677b8e85c1
lu5je0 <lu5je0@gmail.com>
2021-10-21 17:09:05 +0800

    update setup.sh

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ### Installation
 ```sh
-git clone https://github.com/lu5je0/.dotfiles.git
+git clone https://github.com/lu5je0/.dotfiles.git ~/.dotfiles
 bash ~/.dotfiles/scripts/setup.sh
 ```
 
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -30,8 +30,6 @@ ask "Git config?" && ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.vim ~/.vim
 ln -s ~/.dotfiles/.ideavimrc ~/.ideavimrc
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
-# ln -s ~/.dotfiles/.p10k.zsh ~/.p10k.zsh
-ln -s ~/.dotfiles/zsh/lu5je0.zsh-theme ~/.oh-my-zsh/themes/lu5je0.zsh-theme
 
 if [ "$(uname)" = "Darwin" ]; then
     if [[ -f ~/.mac ]]; then
