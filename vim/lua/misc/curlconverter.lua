e893e94f6b0fc884a917cc539e5036a9908c99f3
lu5je0 <lu5je0@gmail.com>
2021-11-19 18:09:47 +0800

    curlconverter.lua

--- vim/lua/misc/curlconverter.lua
+++ vim/lua/misc/curlconverter.lua
@@ -5,7 +5,10 @@ local function firstToUpper(str)
 end
 
 function M.convert()
-  local tl = vim.fn.input("target language:", "python")
+  local tl = vim.fn.input("Target language:", "python")
+  if tl == nil or tl == "" then
+    return
+  end
   local cmd = string.format([[
     :%%!node -e "var curlconverter = require('curlconverter'); const fs = require('fs'); const data = fs.readFileSync('/dev/stdin', 'utf-8'); console.log(curlconverter.to%s(data));"
     set ft=%s
