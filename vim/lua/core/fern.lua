da2ce350c28ce73210a84f5739fe039024ad0bd6
lu5je0 <lu5je0@gmail.com>
2021-11-23 18:09:28 +0800

    +function M.git_checkout_file() +function M.git_reset_file()

--- vim/lua/core/fern.lua
+++ vim/lua/core/fern.lua
@@ -11,6 +11,16 @@ function M.fern_toggle()
   vim.cmd("Fern . -drawer -stay -toggle -keep")
 end
 
+function M.git_checkout_file()
+  vim.cmd("!git checkout -- " .. M.get_cursor_node())
+  vim.cmd("normal r")
+end
+
+function M.git_reset_file()
+  vim.cmd("!git reset -- " .. M.get_cursor_node())
+  vim.cmd("normal r")
+end
+
 function M.preview_toggle()
   plugins_helper.load_plugin("fern-preview.vim")
   vim.call('fern_preview#toggle')
