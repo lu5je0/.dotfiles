8067bddbb155b892d6d5169eeac1e20d742dff9f
lu5je0 <lu5je0@gmail.com>
2021-11-26 08:14:48 +0800

    nvim tree fix nil

--- vim/lua/stack/stack.lua
+++ vim/lua/stack/stack.lua
@@ -2,40 +2,43 @@ local Stack = {}
 local tinsert = table.insert
 
 function Stack:create()
-    local t = {}
-    setmetatable(t, {__index = self})
-    return t
+  local t = {}
+  setmetatable(t, {__index = self})
+  return t
 end
 
 function Stack:push(...)
-    local arg = {...}
-    self.dataTb = self.dataTb or {}
-    if next(arg) then
-        for i = 1, #arg do
-            tinsert(self.dataTb, arg[i])
-        end
+  local arg = {...}
+  self.dataTb = self.dataTb or {}
+  if next(arg) then
+    for i = 1, #arg do
+      tinsert(self.dataTb, arg[i])
     end
+  end
 end
 
 function Stack:pop(num)
-    num = num or 1
-    assert(num > 0, "num必须为正整数")
-    local popTb = {}
-    for _ = 1, num do
-        tinsert(popTb, self.dataTb[#self.dataTb])
-        table.remove(self.dataTb)
-    end
-    return unpack(popTb)
+  num = num or 1
+  assert(num > 0, "num必须为正整数")
+  local popTb = {}
+  for _ = 1, num do
+    tinsert(popTb, self.dataTb[#self.dataTb])
+    table.remove(self.dataTb)
+  end
+  return unpack(popTb)
 end
 
 function Stack:list()
-    for i = 1, #self.dataTb do
-        print(i, self.dataTb[i])
-    end
+  for i = 1, #self.dataTb do
+    print(i, self.dataTb[i])
+  end
 end
 
 function Stack:count()
-    return #self.dataTb
+  if self.dataTb == nil then
+    return 0
+  end
+  return #self.dataTb
 end
 
 return Stack
