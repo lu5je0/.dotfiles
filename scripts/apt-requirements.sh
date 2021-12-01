2f5701a03abb9b5158f51d4268c8ed24a245806c
lu5je0 <lu5je0@gmail.com>
2021-12-01 15:42:38 +0800

    apt luajit

--- scripts/apt-requirements.sh
+++ scripts/apt-requirements.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo apt update
-sudo apt install -y wget curl software-properties-common openjdk-11-jdk neovim zsh htop p7zip p7zip-full unzip build-essential fzf ripgrep nodejs python3 python3-pip tmux git dstat mycli tldr aria2 jq
+sudo apt install -y wget luajit curl software-properties-common openjdk-11-jdk neovim zsh htop p7zip p7zip-full unzip build-essential fzf ripgrep nodejs python3 python3-pip tmux git dstat mycli tldr aria2 jq
