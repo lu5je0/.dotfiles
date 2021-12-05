ce787bd8957f0f7ae8bee37c64b5ccb0afabe964
lu5je0 <lu5je0@gmail.com>
2021-12-06 07:43:39 +0800

    sumneko config

--- vim/lua/core/lspconfig.lua
+++ vim/lua/core/lspconfig.lua
@@ -79,6 +79,9 @@ function M.setup()
       local runtime_path = vim.split(package.path, ";")
       opts.settings = {
         Lua = {
+          -- completion = {
+          --   callSnippet = "Replace",
+          -- },
           runtime = {
             -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
             version = "LuaJIT",
