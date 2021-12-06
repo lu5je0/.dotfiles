b38174a2bb78c91dd3cc1e85063ff5f17d25239a
lu5je0 <lu5je0@gmail.com>
2021-12-06 08:13:04 +0800

    update nvim lsp signs

--- vim/lua/core/lspconfig.lua
+++ vim/lua/core/lspconfig.lua
@@ -41,7 +41,7 @@ function M.setup()
       update_in_insert = true,
     }
   )
-  local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+  local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
   for type, icon in pairs(signs) do
     local hl = "DiagnosticSign" .. type
     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
