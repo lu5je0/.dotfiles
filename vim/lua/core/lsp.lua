6c58ac04d3baa34ae0230b141123df12dbfe5b09
lu5je0 <lu5je0@gmail.com>
2021-12-08 17:37:28 +0800

    nvim lsp show diagnostic on CursorHold

--- vim/lua/core/lsp.lua
+++ vim/lua/core/lsp.lua
@@ -8,6 +8,26 @@ M.on_attach = function(client, bufnr)
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
   local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
+  -- Show diagnostics in a pop-up window on hover
+  _G.LspDiagnosticsPopupHandler = function()
+    local current_cursor = vim.api.nvim_win_get_cursor(0)
+    local last_popup_cursor = vim.w.lsp_diagnostics_last_cursor or {nil, nil}
+
+    -- Show the popup diagnostics window,
+    -- but only once for the current cursor location (unless moved afterwards).
+    if not (current_cursor[1] == last_popup_cursor[1] and current_cursor[2] == last_popup_cursor[2]) then
+      vim.w.lsp_diagnostics_last_cursor = current_cursor
+      -- vim.diagnostic.open_float(0, {scope="cursor"})   -- for neovim 0.6.0+, replaces show_{line,position}_diagnostics
+      vim.lsp.diagnostic.show_position_diagnostics({show_header = false})
+    end
+  end
+  vim.cmd [[
+  augroup LSPDiagnosticsOnHover
+    autocmd!
+    autocmd CursorHold *   lua _G.LspDiagnosticsPopupHandler()
+  augroup END
+  ]]
+
   -- Enable completion triggered by <c-x><c-o>
   buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
