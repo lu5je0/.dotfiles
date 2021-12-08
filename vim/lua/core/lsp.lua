14d3e714c3baa051b01e181036f5c2e77670e4aa
lu5je0 <lu5je0@gmail.com>
2021-12-09 07:05:41 +0800

    update

--- vim/lua/core/lsp.lua
+++ vim/lua/core/lsp.lua
@@ -64,10 +64,6 @@ M.lua_setting = {
       version = "LuaJIT",
     },
     workspace = {
-      library = {
-        [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-        [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true
-      },
       maxPreload = 100000,
       preloadFileSize = 10000
     },
