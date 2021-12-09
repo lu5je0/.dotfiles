5cb988e0cf74ff9d537d9e3535efc07af7cc6ac7
lu5je0 <lu5je0@gmail.com>
2021-12-09 12:44:49 +0800

    M.node_global_modules_path

--- vim/lua/misc/curlconverter.lua
+++ vim/lua/misc/curlconverter.lua
@@ -4,15 +4,22 @@ local function firstToUpper(str)
     return (str:gsub("^%l", string.upper))
 end
 
+M.node_global_modules_path = ""
+
 function M.convert()
   local tl = vim.fn.input("Target language:", "python")
   if tl == nil or tl == "" then
     return
   end
+
+  if M.node_global_modules_path == "" then
+    M.node_global_modules_path = vim.fn.system("npm root --quiet -g"):sub(1, -2)
+  end
+
   local cmd = string.format([[
-    :%%!node -e "var curlconverter = require('/usr/local/lib/node_modules/curlconverter'); const fs = require('fs'); const data = fs.readFileSync('/dev/stdin', 'utf-8'); console.log(curlconverter.to%s(data));"
+    :%%!node -e "var curlconverter = require('%s/curlconverter'); const fs = require('fs'); const data = fs.readFileSync('/dev/stdin', 'utf-8'); console.log(curlconverter.to%s(data));"
     set ft=%s
-  ]], firstToUpper(tl), tl);
+  ]], M.node_global_modules_path, firstToUpper(tl), tl);
 
   vim.cmd(cmd)
 end
