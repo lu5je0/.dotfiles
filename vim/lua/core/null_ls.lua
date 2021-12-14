a7fa9048a2bfa4790b9f562423790440a9162231
lu5je0 <lu5je0@gmail.com>
2021-12-14 22:58:44 +0800

    feat: disable null_ls eslint

--- vim/lua/core/null_ls.lua
+++ vim/lua/core/null_ls.lua
@@ -3,7 +3,7 @@ local null_ls = require("null-ls")
 null_ls.setup({
     sources = {
         require("null-ls").builtins.formatting.stylua,
-        require("null-ls").builtins.diagnostics.eslint,
+        -- require("null-ls").builtins.diagnostics.eslint,
         require("null-ls").builtins.completion.spell,
     },
 })
