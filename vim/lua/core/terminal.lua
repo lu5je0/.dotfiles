e5e4b9c7c2838f81cdfe131ccca0523d49361a06
lu5je0 <lu5je0@gmail.com>
2021-12-21 05:40:28 +0800

    feat: toggleterm change direction

--- vim/lua/core/terminal.lua
+++ vim/lua/core/terminal.lua
@@ -11,6 +11,17 @@ M.sendToTerminal = function(cmd, opts)
   vim.cmd(v_cmd)
 end
 
+M.direction = 'float'
+
+M.toggle = function()
+  vim.cmd('ToggleTerm direction=' .. M.direction)
+end
+
+M.change_terminal_direction = function(direction)
+  M.direction = direction
+  M.toggle()
+end
+
 M.runSelectInTerminal = function()
   M.sendToTerminal(vim.fn['visual#visual_selection']())
 end
@@ -31,29 +42,28 @@ M.setup = function()
     start_in_insert = true,
     insert_mappings = true, -- whether or not the open mapping applies in insert mode
     persist_size = true,
-    direction = 'float',
     close_on_exit = true, -- close the terminal window when the process exits
     shell = vim.o.shell, -- change the default shell
   }
   vim.cmd[[
-  imap <silent> <m-i> <ESC>:ToggleTerm<CR>
-  imap <silent> <d-i> <ESC>:ToggleTerm<CR>
+  imap <silent> <m-i> <ESC>:lua require('core.terminal').toggle()<CR>
+  imap <silent> <d-i> <ESC>:lua require('core.terminal').toggle()<CR>
 
-  tmap <silent> <m-i> <c-\><c-n>:ToggleTerm<CR>
-  tmap <silent> <d-i> <c-\><c-n>:ToggleTerm<CR>
+  tmap <silent> <m-i> <c-\><c-n>:lua require('core.terminal').toggle()<CR>
+  tmap <silent> <d-i> <c-\><c-n>:lua require('core.terminal').toggle()<CR>
   
-  tmap <silent> <c-w>L <c-\><c-n><m-i>:TermExec cmd="echo 'L'" direction='vertical' go_back=0<CR>
-  tmap <silent> <c-w>J <c-\><c-n><m-i>:TermExec cmd="echo 'J'" direction='horizontal' go_back=0<CR>
-  tmap <silent> <c-w>F <c-\><c-n><m-i>:TermExec cmd="echo 'F'" direction='float' go_back=0<CR>
+  nmap <silent> <m-i> :lua require('core.terminal').toggle()<CR>
+  nmap <silent> <d-i> :lua require('core.terminal').toggle()<CR>
+  
+  tmap <silent> <c-w>L <c-\><c-n><m-i>:lua require('core.terminal').change_terminal_direction('vertical')<CR>
+  tmap <silent> <c-w>J <c-\><c-n><m-i>:lua require('core.terminal').change_terminal_direction('horizontal')<CR>
+  tmap <silent> <c-w>F <c-\><c-n><m-i>:lua require('core.terminal').change_terminal_direction('float')<CR>
   
   tmap <silent> <c-h> <c-\><c-n><c-w>h
   tmap <silent> <c-l> <c-\><c-n><c-w>l
   tmap <silent> <c-j> <c-\><c-n><c-w>j
   tmap <silent> <c-k> <c-\><c-n><c-w>k
   tmap <silent> <c-q> <c-\><c-n>
-
-  nmap <silent> <m-i> :ToggleTerm<CR>
-  nmap <silent> <d-i> :ToggleTerm<CR>
   
   augroup TerminalConfig
     autocmd!
