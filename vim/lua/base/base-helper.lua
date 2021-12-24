aa877810490d188bbd0510adb3075c46a6fedcfc
lu5je0 <lu5je0@gmail.com>
2021-12-25 04:09:50 +0800

    feat: update

--- vim/lua/base/base-helper.lua
+++ vim/lua/base/base-helper.lua
@@ -77,7 +77,7 @@ M.exit_vim = function()
   if #unsave_buffers ~= 0 then
     msg = 'The change of the following buffers will be discarded.\n'
     for _, buffer in ipairs(unsave_buffers) do
-      local name = vim.fn.fnamemodify(buffer.name, ':t')
+      local name = require('nvim-web-devicons').get_icon(buffer.name, string.split(buffer.name, '.')[-1]) .. ' ' .. vim.fn.fnamemodify(buffer.name, ':t')
       if name == '' then
         name = '[No Name] ' .. buffer.bufnr
       end
