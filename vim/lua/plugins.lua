22b09fb2cb90973f5773b8f8797e43f3436a41dc
lu5je0 <lu5je0@gmail.com>
2021-12-25 16:12:24 +0800

    feat: update indent-blankline color

--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -482,9 +482,16 @@ return packer.startup(function()
     'lukas-reineke/indent-blankline.nvim',
     setup = function()
       vim.g.indent_blankline_char = '▏'
-      vim.g.indentLine_fileTypeExclude = { 'undotree', 'vista', 'git', 'diff', 'translator', 'help', 'packer', 'lsp-installer', 'toggleterm' , 'confirm' }
+      vim.g.indentLine_fileTypeExclude = { 'undotree', 'vista', 'git', 'diff', 'translator', 'help', 'packer', 'lsp-installer', 'toggleterm', 'confirm' }
       vim.g.indent_blankline_show_first_indent_level = false
       vim.g.indent_blankline_show_trailing_blankline_indent = false
+      vim.cmd [[highlight IndentBlanklineIndent guifg=#373C44 gui=nocombine]]
+      require('indent_blankline').setup({
+        space_char_blankline = ' ',
+        char_highlight_list = {
+          'IndentBlanklineIndent',
+        },
+      })
     end,
     opt = true,
   })
