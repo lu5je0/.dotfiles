30297728f9e55c8d37b959c55a87e0d8b8b2dd99
lu5je0 <lu5je0@gmail.com>
2022-01-17 11:16:25 +0800

    chore: update treesiter.lua

--- vim/lua/core/treesiter.lua
+++ vim/lua/core/treesiter.lua
@@ -12,17 +12,52 @@ require('nvim-treesitter.configs').setup({
   },
 })
 
-local parsers = require('nvim-treesitter.parsers').available_parsers()
-table.remove_by_value(parsers, 'markdown')
-vim.cmd(([[
+local define_modules = require('nvim-treesitter').define_modules
+local query = require('nvim-treesitter.query')
+
+local foldmethod_backups = {}
+local foldexpr_backups = {}
+
+local fold_skip_filetypes = { 'markdown' }
+
+define_modules({
+  folding = {
+    enable = true,
+    attach = function(bufnr)
+      if table.contain(fold_skip_filetypes, vim.bo.filetype) then
+        return
+      end
+      -- Fold settings are actually window based...
+      foldmethod_backups[bufnr] = vim.wo.foldmethod
+      foldexpr_backups[bufnr] = vim.wo.foldexpr
+      vim.wo.foldmethod = 'expr'
+      vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
+    end,
+    detach = function(bufnr)
+      vim.wo.foldmethod = foldmethod_backups[bufnr]
+      vim.wo.foldexpr = foldexpr_backups[bufnr]
+      foldmethod_backups[bufnr] = nil
+      foldexpr_backups[bufnr] = nil
+    end,
+    is_supported = query.has_folds,
+  },
+})
+
+vim.cmd([[
 hi TSPunctBracket guifg=#ABB2BF
 
 " hi! TSTitle ctermfg=168 guifg=#e06c75
 " hi! link TSURI markdownUrl
 " hi! link TSStrong markdownBold
+]])
 
-augroup ts_fold_fix
-    autocmd!
-    autocmd Filetype %s setlocal foldmethod=expr | setlocal foldexpr=nvim_treesitter#foldexpr()
-augroup END
-]]):format(table.concat(parsers, ',')))
+-- local parsers = require('nvim-treesitter.parsers').available_parsers()
+-- table.remove_by_value(parsers, 'markdown')
+-- vim.cmd(([[
+--
+-- augroup ts_fold_fix
+--     autocmd!
+--     autocmd Filetype %s setlocal foldmethod=expr | setlocal foldexpr=nvim_treesitter#foldexpr()
+-- augroup END
+-- ]]):format(table.concat(parsers, ',')))
+
