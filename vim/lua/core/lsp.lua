9cbbfa87bc84ce08c912fe85d9210a9201b4d705
lu5je0 <lu5je0@gmail.com>
2022-01-23 19:11:54 +0800

    1.ideavimrc <leader><space> 2.disable lsp error popup

--- vim/lua/core/lsp.lua
+++ vim/lua/core/lsp.lua
@@ -65,28 +65,28 @@ local function on_attach(client, bufnr)
   end
 
   -- Show diagnostics in a pop-up window on hover
-  _G.LspDiagnosticsPopupHandler = function()
-    local current_cursor = vim.api.nvim_win_get_cursor(0)
-    local last_popup_cursor = vim.w.lsp_diagnostics_last_cursor or { nil, nil }
-
-    -- Show the popup diagnostics window,
-    -- but only once for the current cursor location (unless moved afterwards).
-    if not (current_cursor[1] == last_popup_cursor[1] and current_cursor[2] == last_popup_cursor[2]) then
-      vim.w.lsp_diagnostics_last_cursor = current_cursor
-      if vim.fn.has('nvim-0.6') == 1 then
-        vim.diagnostic.open_float(0, { scope = 'cursor' }) -- for neovim 0.6.0+, replaces show_{line,position}_diagnostics
-      else
-        ---@diagnostic disable-next-line: deprecated
-        vim.lsp.diagnostic.show_position_diagnostics({ show_header = false })
-      end
-    end
-  end
-  vim.cmd([[
-  augroup LSPDiagnosticsOnHover
-    autocmd!
-    autocmd CursorHold * lua _G.LspDiagnosticsPopupHandler()
-  augroup END
-  ]])
+  -- _G.LspDiagnosticsPopupHandler = function()
+  --   local current_cursor = vim.api.nvim_win_get_cursor(0)
+  --   local last_popup_cursor = vim.w.lsp_diagnostics_last_cursor or { nil, nil }
+  --
+  --   -- Show the popup diagnostics window,
+  --   -- but only once for the current cursor location (unless moved afterwards).
+  --   if not (current_cursor[1] == last_popup_cursor[1] and current_cursor[2] == last_popup_cursor[2]) then
+  --     vim.w.lsp_diagnostics_last_cursor = current_cursor
+  --     if vim.fn.has('nvim-0.6') == 1 then
+  --       vim.diagnostic.open_float(0, { scope = 'cursor' }) -- for neovim 0.6.0+, replaces show_{line,position}_diagnostics
+  --     else
+  --       ---@diagnostic disable-next-line: deprecated
+  --       vim.lsp.diagnostic.show_position_diagnostics({ show_header = false })
+  --     end
+  --   end
+  -- end
+  -- vim.cmd([[
+  -- augroup LSPDiagnosticsOnHover
+  --   autocmd!
+  --   autocmd CursorHold * lua _G.LspDiagnosticsPopupHandler()
+  -- augroup END
+  -- ]])
 
   -- Enable completion triggered by <c-x><c-o>
   buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
