bd5daf34452e0751d3f47e9043e607ab46b73775
lu5je0 <lu5je0@gmail.com>
2022-01-24 12:17:59 +0800

    fix(lsp json)

--- vim/lua/patch.lua
+++ vim/lua/patch.lua
@@ -1,14 +1,16 @@
 -- 让lsp能够attach到未命名文件
 function _G.lsp_attach_on_no_filename()
+  local buf_name = vim.api.nvim_buf_get_name(0)
+  if buf_name ~= nil and buf_name ~= '' then
+    return
+  end
+
   if #vim.lsp.buf_get_clients(0) ~= 0 then
     return
   end
 
-  local buf_name = vim.api.nvim_buf_get_name(0)
-  if buf_name == nil or buf_name == '' then
-    vim.api.nvim_buf_set_name(0, '/tmp/tmp' .. vim.bo.filetype)
-    vim.cmd("LspStart")
-  end
+  vim.api.nvim_buf_set_name(0, '/tmp/tmp' .. vim.bo.filetype)
+  vim.cmd("LspStart")
   vim.api.nvim_buf_set_name(0, buf_name)
 end
 
