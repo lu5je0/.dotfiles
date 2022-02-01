f803a63aa5cf5916588686318b0be02ae3012ae5
lu5je0 <lu5je0@gmail.com>
2022-02-01 16:39:51 +0800

    fix: CronParser

--- vim/lua/commands.lua
+++ vim/lua/commands.lua
@@ -1,4 +1,4 @@
 vim.cmd([[
 command! -nargs=0 CurlConvert lua require("misc/curlconverter").convert()
-command! -nargs=* CronParser call luaeval(\"require('misc/cron-parser').parse_line(_A)\", [<f-args>])
+command! -nargs=* CronParser call luaeval("require('misc/cron-parser').parse_line(_A)", [<f-args>])
 ]])
