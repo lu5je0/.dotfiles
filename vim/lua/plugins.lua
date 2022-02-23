351cb03a225723ec4c35c04b2c718530b8a6bb7e
lu5je0 <lu5je0@gmail.com>
2022-02-23 15:36:53 +0800

    feat: remove vim-eft

--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -59,29 +59,29 @@ return packer.startup(function(use)
     end,
   }
 
-  use {
-    'hrsh7th/vim-eft',
-    config = function()
-      vim.cmd([[
-      nmap ; <Plug>(eft-repeat)
-      xmap ; <Plug>(eft-repeat)
-
-      nmap f <Plug>(eft-f)
-      xmap f <Plug>(eft-f)
-      omap f <Plug>(eft-f)
-      nmap F <Plug>(eft-F)
-      xmap F <Plug>(eft-F)
-      omap F <Plug>(eft-F)
-
-      nmap t <Plug>(eft-t)
-      xmap t <Plug>(eft-t)
-      omap t <Plug>(eft-t)
-      nmap T <Plug>(eft-T)
-      xmap T <Plug>(eft-T)
-      omap T <Plug>(eft-T)
-      ]])
-    end,
-  }
+  -- use {
+  --   'hrsh7th/vim-eft',
+  --   config = function()
+  --     vim.cmd([[
+  --     nmap ; <Plug>(eft-repeat)
+  --     xmap ; <Plug>(eft-repeat)
+  --
+  --     nmap f <Plug>(eft-f)
+  --     xmap f <Plug>(eft-f)
+  --     omap f <Plug>(eft-f)
+  --     nmap F <Plug>(eft-F)
+  --     xmap F <Plug>(eft-F)
+  --     omap F <Plug>(eft-F)
+  --
+  --     nmap t <Plug>(eft-t)
+  --     xmap t <Plug>(eft-t)
+  --     omap t <Plug>(eft-t)
+  --     nmap T <Plug>(eft-T)
+  --     xmap T <Plug>(eft-T)
+  --     omap T <Plug>(eft-T)
+  --     ]])
+  --   end,
+  -- }
 
   use {
     'kyazdani42/nvim-web-devicons',
