e46e5fdb94d95b9fbbd648d8bc35f647a06b62a7
lu5je0 <lu5je0@gmail.com>
2022-03-25 12:51:52 +0800

    feat: update nvim-tree.lua

--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -509,7 +509,7 @@ return packer.startup(function(use)
     requires = 'kyazdani42/nvim-web-devicons',
     keys = { '<leader>e', '<leader>fe' },
     opt = true,
-    branch = 'dev',
+    branch = 'dev-20220326',
     config = function()
       require('core.nvimtree').setup()
     end,
