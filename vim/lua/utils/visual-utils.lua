99e3f4c0fa8b893eacde70357652c263e2921ed8
lu5je0 <lu5je0@gmail.com>
2022-03-30 18:54:40 +0800

    feat: var_naming_converter

--- vim/lua/utils/visual-utils.lua
+++ vim/lua/utils/visual-utils.lua
@@ -1,7 +1,18 @@
-local M
+local M = {}
 
 function M.selected_text()
-  return ''
+  return vim.fn['visual#visual_selection']()
+end
+
+function M.replace_with(text)
+  local cmd = [[
+  function! TempString(s)
+    return '%s'
+  endfunction
+  ]]
+  cmd = cmd:format(text)
+  vim.cmd(cmd)
+  vim.fn['ReplaceSelect']('TempString')
 end
 
 return M
