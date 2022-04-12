d4cf31ac218b21edb16d80df00d6c8e38f84a4f0
lu5je0 <lu5je0@gmail.com>
2022-04-12 21:55:02 +0800

    chore: nvim_add_user_command -> nvim_create_user_command

--- vim/lua/commands.lua
+++ vim/lua/commands.lua
@@ -1,16 +1,16 @@
-vim.api.nvim_add_user_command('CronParser', function(t)
+vim.api.nvim_create_user_command('CronParser', function(t)
   require('misc/cron-parser').parse_line(t.fargs[1])
 end, { force = true, nargs='*' })
 
-vim.api.nvim_add_user_command('CurlConvert', function()
+vim.api.nvim_create_user_command('CurlConvert', function()
   require("misc/curlconverter").convert()
 end, { force = true })
 
 -- json-helper
-vim.api.nvim_add_user_command('JsonCompress', function()
+vim.api.nvim_create_user_command('JsonCompress', function()
   require('misc.json-helper').compress()
 end, { force = true })
 
-vim.api.nvim_add_user_command('JsonFormat', function()
+vim.api.nvim_create_user_command('JsonFormat', function()
   require('misc.json-helper').format()
 end, { force = true })
