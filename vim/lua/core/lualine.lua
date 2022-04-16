d6d7c83ba911af017fe17e7a38a75d8ac348bae7
lu5je0 <lu5je0@gmail.com>
2022-04-16 19:46:11 +0800

    feat: hide lsp name

--- vim/lua/core/lualine.lua
+++ vim/lua/core/lualine.lua
@@ -259,27 +259,34 @@ end
 ins_right {
   -- Lsp server name .
   function()
-    local msg = nil
-    local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
     local clients = vim.lsp.get_active_clients()
-    if next(clients) == nil then
-      return ' ' .. msg
-    end
-    for _, client in ipairs(clients) do
-      local filetypes = client.config.filetypes
-      if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-        if client.name == 'null-ls' then
-          goto continue
-        end
-        return ' ' .. client.name
-      end
-      ::continue::
-    end
-    if msg == nil then
-      return ''
+    if next(clients) ~= nil then
+      return ''
     else
-      return ' ' .. msg
+      return ''
     end
+
+    -- local msg = nil
+    -- local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+    -- local clients = vim.lsp.get_active_clients()
+    -- if next(clients) == nil then
+    --   return ' ' .. msg
+    -- end
+    -- for _, client in ipairs(clients) do
+    --   local filetypes = client.config.filetypes
+    --   if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+    --     if client.name == 'null-ls' then
+    --       goto continue
+    --     end
+    --     return ' ' .. client.name
+    --   end
+    --   ::continue::
+    -- end
+    -- if msg == nil then
+    --   return ''
+    -- else
+    --   return ' ' .. msg
+    -- end
   end,
   color = { fg = colors.cyan, gui = 'bold' },
   cond = conditions.lsp_cond,
