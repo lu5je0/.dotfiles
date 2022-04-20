1024470c3b7635c1aa44e3f98ad0a76c9544067a
lu5je0 <lu5je0@gmail.com>
2022-04-21 02:11:38 +0800

    feat: 优化win im.lua

--- vim/lua/lu5je0/misc/im/win/im.lua
+++ vim/lua/lu5je0/misc/im/win/im.lua
@@ -1,15 +1,22 @@
 local M = {}
 
+local group = vim.api.nvim_create_augroup('ime-status', { clear = true })
+local path = vim.fn.stdpath('config')
 
-M.boostrap = function()
-  local group = vim.api.nvim_create_augroup('ime-status', { clear = true })
-  local path = vim.fn.stdpath('config')
+local function disable_ime()
+  io.popen(path .. '/lib/toDisableIME.exe')
+end
+
+local function enable_ime()
+  io.popen(path .. '/lib/toEnableIME.exe')
+end
+
+local function create_autocmd()
   vim.api.nvim_create_autocmd('InsertLeave', {
     group = group,
     pattern = { '*' },
     callback = function()
-      print(path .. '/lib/toDisableIME.exe')
-      io.popen(path .. '/lib/toDisableIME.exe')
+      disable_ime()
     end
   })
 
@@ -17,9 +24,22 @@ M.boostrap = function()
     group = group,
     pattern = { '*' },
     callback = function()
-      io.popen(path .. '/lib/toEnableIME.exe')
+      enable_ime()
     end
   })
+end
+
+local function defer_normal_keep()
+  vim.defer_fn(function()
+    if vim.api.nvim_get_mode().mode == 'n' then
+      disable_ime()
+    end
+  end, 20)
+end
+
+M.boostrap = function()
+  create_autocmd()
+  defer_normal_keep()
 end
 
 return M
