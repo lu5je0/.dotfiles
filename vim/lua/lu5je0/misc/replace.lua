0b732ea55b996b853c741d0712fe0a1451007021
lu5je0 <lu5je0@gmail.com>
2022-05-06 16:54:51 +0800

    feat: update replace.lua

--- vim/lua/lu5je0/misc/replace.lua
+++ vim/lua/lu5je0/misc/replace.lua
@@ -10,17 +10,20 @@ local function replace(mode)
     ---@diagnostic disable-next-line: missing-parameter
     pattern = vim.fn.expand('<cword>')
   elseif mode == 'v' then
-    pattern = require('lu5je0.core.visual').selected_text()
+    pattern = require('lu5je0.core.visual').selected_text_by_yank()
   end
 
-  vim.ui.input({}, function(repl)
+  vim.ui.input({ prompt = string.format('Replace "%s" with: ', pattern) }, function(repl)
     if repl == nil or repl == '' then
       return
     end
 
+    pattern = string.escape_pattern(pattern)
     for i, line_text in ipairs(vim.api.nvim_buf_get_lines(0, 0, -1, false)) do
-      line_text = string.gsub(line_text, pattern, repl)
-      vim.api.nvim_buf_set_lines(0, i - 1, i, false, { line_text })
+      local replaced_text = string.gsub(line_text, pattern, repl)
+      if replaced_text ~= line_text then
+        vim.api.nvim_buf_set_lines(0, i - 1, i, false, { replaced_text })
+      end
     end
 
     cursor_utils.goto_saved_position()
