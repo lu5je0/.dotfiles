89a958aca29b2238424bcc6796cdb4c90ae9a84f
lu5je0 <lu5je0@gmail.com>
2022-06-11 18:37:29 +0800

    update patch.lua

--- vim/lua/lu5je0/patch.lua
+++ vim/lua/lu5je0/patch.lua
@@ -1,31 +1,30 @@
 _G.__patch = {}
 local patch = _G.__patch
+local au_group = require('lu5je0.autocmds').default_group
 
 -- 让lsp能够attach到未命名文件
-function patch.lsp_attach_on_no_filename()
-  local buf_name = vim.api.nvim_buf_get_name(0)
-  if buf_name ~= nil and buf_name ~= '' then
-    return
-  end
+vim.api.nvim_create_autocmd('FileType', {
+  group = au_group,
+  pattern = { 'json', 'python', 'sql' },
+  callback = function()
+    local buf_name = vim.api.nvim_buf_get_name(0)
+    if buf_name ~= nil and buf_name ~= '' then
+      return
+    end
 
-  if #vim.lsp.buf_get_clients(0) ~= 0 then
-    return
-  end
+    if #vim.lsp.buf_get_clients(0) ~= 0 then
+      return
+    end
 
-  vim.api.nvim_buf_set_name(0, '/tmp/tmp.' .. vim.bo.filetype)
-  vim.cmd('LspStart')
-  require('null-ls/client').try_add(0)
-  -- if buf_name == nil or buf_name == '' then
-  --   buf_name = '[No Name]'
-  -- end
-  vim.api.nvim_buf_set_name(0, buf_name)
-end
-vim.cmd([[
-augroup confirm_lsp_attach
-    autocmd!
-    autocmd FileType json,python,sql lua _G.__patch.lsp_attach_on_no_filename()
-augroup END
-]])
+    vim.api.nvim_buf_set_name(0, '/tmp/tmp.' .. vim.bo.filetype)
+    vim.cmd('LspStart')
+    require('null-ls/client').try_add(0)
+    -- if buf_name == nil or buf_name == '' then
+    --   buf_name = '[No Name]'
+    -- end
+    vim.api.nvim_buf_set_name(0, buf_name)
+  end,
+})
 
 -- 修复set filetype后无法使用treesitter fold
 function patch.fold_patch()
