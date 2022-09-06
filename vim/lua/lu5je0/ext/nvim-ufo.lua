cf792186eaf03d89dcd0dc278a9ba919eead1d90
lu5je0 <lu5je0@gmail.com>
2022-09-06 10:47:40 +0800

    update nvim-ufo config

--- vim/lua/lu5je0/ext/nvim-ufo.lua
+++ vim/lua/lu5je0/ext/nvim-ufo.lua
@@ -29,6 +29,14 @@ local fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate
   return newVirtText
 end
 
+-- vim.o.foldcolumn = '1'
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+
+vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+
 require('ufo').setup({
   provider_selector = function(bufnr, filetype, buftype)
     if parsers.get_parser(bufnr) then
