48eb7b3f33fabeb879083aea4111b12ecd2bf996
lu5je0 <lu5je0@gmail.com>
2022-09-15 15:20:46 +0800

    update filetype.lua

--- vim/lua/lu5je0/filetype.lua
+++ vim/lua/lu5je0/filetype.lua
@@ -4,12 +4,13 @@ vim.filetype.add {
   },
   filename = {
     -- Set the filetype of files named "MyBackupFile" to lua
-    ['.bashrc'] = 'sh',
-    ['.zshrc'] = 'sh',
-    ['zshrc'] = 'sh',
-    ['bashrc'] = 'sh',
-    ['.ohmyenv'] = 'sh',
+    ['.bashrc'] = 'bash',
+    ['.zshrc'] = 'bash',
+    ['zshrc'] = 'bash',
+    ['bashrc'] = 'bash',
+    ['.ohmyenv'] = 'bash',
     ['crontab'] = 'crontab',
+    ['aria2.conf'] = 'dosini',
   },
   pattern = {
     ['.*%.tmux.conf'] = 'tmux',
