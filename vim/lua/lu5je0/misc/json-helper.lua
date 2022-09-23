ee0d1e88edd42021f85be3fa7ec03515fc565832
lu5je0 <lu5je0@gmail.com>
2022-09-23 10:59:30 +0800

    feat: json-helper jq

--- vim/lua/lu5je0/misc/json-helper.lua
+++ vim/lua/lu5je0/misc/json-helper.lua
@@ -8,6 +8,10 @@ M.format = function()
   vim.cmd(':%!jq')
 end
 
+M.jq = function (args)
+  vim.cmd(':%!jq ' .. args)
+end
+
 M.setup = function()
   vim.api.nvim_create_user_command('JsonCompress', function()
     M.compress()
@@ -16,6 +20,15 @@ M.setup = function()
   vim.api.nvim_create_user_command('JsonFormat', function()
     M.format()
   end, { force = true })
+  
+  vim.api.nvim_create_user_command('Json', function()
+    vim.cmd('set ft=json')
+    M.format()
+  end, { force = true })
+  
+  vim.api.nvim_create_user_command('Jq', function(args)
+    M.jq(args.args)
+  end, { force = true, nargs = '*' })
 end
 
 return M
