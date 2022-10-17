96c404799656296889ef5edc7a81422aa51f8617
lu5je0 <lu5je0@gmail.com>
2022-10-17 10:18:08 +0800

    cmp-nvim-lsp: update_capabilities -> default_capabilities

--- vim/lua/lu5je0/ext/lspconfig/lsp.lua
+++ vim/lua/lu5je0/ext/lspconfig/lsp.lua
@@ -58,7 +58,7 @@ end
 
 local function config()
   -- nvim-cmp
-  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
   -- nvim-ufo
   capabilities.textDocument.foldingRange = {
