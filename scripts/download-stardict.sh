c656da3a58450203918b53ac38ec3dabca4a681e
lu5je0 <lu5je0@gmail.com>
2022-11-04 23:19:26 +0800

    update download-stardict.sh

--- scripts/download-stardict.sh
+++ scripts/download-stardict.sh
@@ -3,7 +3,7 @@ type unzip >/dev/null 2>&1 || { echo >&2 "unzip not installed. Aborting."; exit 
 type wget >/dev/null 2>&1 || { echo >&2 "wget not installed. Aborting."; exit 1; }
 
 rm stardict.7z*
-wget https://github.com/lu5je0/wd/releases/download/1.0/stardict.7z
+curl -L -o stardict.7z https://github.com/lu5je0/wd/releases/download/1.0/stardict.7z
 7za x stardict.7z
 mkdir -p ~/.local/share/stardict
 mv stardict.db ~/.local/share/stardict
