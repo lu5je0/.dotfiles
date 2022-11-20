bc07f8ece5e56fb549cb333f8005b366da31dcee
lu5je0 <lu5je0@gmail.com>
2022-11-20 15:00:11 +0800

    disable postfix snippets

--- vim/lua/lu5je0/ext/luasnip.lua
+++ vim/lua/lu5je0/ext/luasnip.lua
@@ -20,75 +20,9 @@ local function keymap()
   require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./snippets/" } })
 end
 
-local function snippets()
-  local ls = require "luasnip"
-  local s = ls.snippet
-  local sn = ls.snippet_node
-  local isn = ls.indent_snippet_node
-  local t = ls.text_node
-  local i = ls.insert_node
-  local f = ls.function_node
-  local c = ls.choice_node
-  local d = ls.dynamic_node
-  local r = ls.restore_node
-  local events = require "luasnip.util.events"
-  local ai = require "luasnip.nodes.absolute_indexer"
-  local extras = require "luasnip.extras"
-  local fmt = extras.fmt
-  local m = extras.m
-  local l = extras.l
-  local postfix = require "luasnip.extras.postfix".postfix
-
-  ls.add_snippets("all", {
-    postfix({ trig = ".parr", match_pattern = "[%w%.%_%-%(%)]+$" }, {
-      t("("),
-      f(function(_, parent)
-        return parent.snippet.env.POSTFIX_MATCH
-      end, {}),
-      t(")"),
-    })
-  })
-
-  ls.add_snippets("all", {
-    postfix({ trig = ".argg", match_pattern = "[%w%.%_%-%(%)]+$" }, {
-      i(1, ""),
-      t("("),
-      f(function(_, parent)
-        return parent.snippet.env.POSTFIX_MATCH
-      end, {}),
-      t(")"),
-    })
-  })
-
-  ls.add_snippets("lua", {
-    postfix({ trig = ".varr", match_pattern = "^ +(.+)$" }, {
-      t("local "), i(1, ""), t(" = "),
-      f(function(_, parent)
-        return parent.snippet.env.POSTFIX_MATCH
-      end, {}),
-    })
-  })
-
-  ls.add_snippets("python", {
-    postfix({ trig = ".varr", match_pattern = "^ +(.+)$" }, {
-      i(1, ""), t(" = "),
-      f(function(_, parent)
-        return parent.snippet.env.POSTFIX_MATCH
-      end, {}),
-    })
-  })
-
-  -- ls.add_snippets("all", {
-  --   s("ternary", {
-  --     -- equivalent to "${1:cond} ? ${2:then} : ${3:else}"
-  --     i(1, "cond"), t(" ? "), i(2, "then"), t(" : "), i(3, "else")
-  --   })
-  -- })
-end
-
 M.setup = function()
   keymap()
-  snippets()
+  -- require('lu5je0.ext.luasnips.snippets')
 end
 
 
---
+++ vim/lua/lu5je0/ext/luasnips/snippets.lua
@@ -0,0 +1,80 @@
+---@diagnostic disable: unused-local
+
+local ls = require "luasnip"
+local s = ls.snippet
+local sn = ls.snippet_node
+local isn = ls.indent_snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local r = ls.restore_node
+local events = require "luasnip.util.events"
+local ai = require "luasnip.nodes.absolute_indexer"
+local extras = require "luasnip.extras"
+local fmt = extras.fmt
+local m = extras.m
+local l = extras.l
+local postfix = require "luasnip.extras.postfix".postfix
+
+-- ls.add_snippets("all", {
+--   postfix({ trig = ".parr", match_pattern = "[%w%.%_%-%(%)]+$" }, {
+--     t("("),
+--     f(function(_, parent)
+--       return parent.snippet.env.POSTFIX_MATCH
+--     end, {}),
+--     t(")"),
+--   })
+-- })
+--
+-- ls.add_snippets("all", {
+--   postfix({ trig = ".argg", match_pattern = "[%w%.%_%-%(%)]+$" }, {
+--     i(1, ""),
+--     t("("),
+--     f(function(_, parent)
+--       return parent.snippet.env.POSTFIX_MATCH
+--     end, {}),
+--     t(")"),
+--   })
+-- })
+--
+-- ls.add_snippets("lua", {
+--   postfix({ trig = ".local", match_pattern = "^ +(.+)$" }, {
+--     t("local "), i(1, ""), t(" = "),
+--     f(function(_, parent)
+--       return parent.snippet.env.POSTFIX_MATCH
+--     end, {}),
+--   })
+-- })
+--
+--
+-- -- for i = 1, 10, 1 do
+-- --
+-- -- end
+-- ls.add_snippets("all", {
+--   postfix({ trig = ".fori", match_pattern = " %d+$" }, {
+--     f(function(_, parent)
+--       return ("for i = 1, %d, 1 do"):format(parent.snippet.env.POSTFIX_MATCH)
+--     end, {}), t(""),
+--     t("  "), i(""), t(""),
+--     t("end"),
+--   })
+-- })
+
+-- ls.add_snippets("python", {
+--   postfix({ trig = ".varr", match_pattern = "^ +(.+)$" }, {
+--     i(1, ""), t(" = "),
+--     f(function(_, parent)
+--       return parent.snippet.env.POSTFIX_MATCH
+--     end, {}),
+--   })
+-- })
+
+-- ls.add_snippets("all", {
+--   s("ternary", {
+--     -- equivalent to "${1:cond} ? ${2:then} : ${3:else}"
+--     i(1, "cond"), t(" ? "), i(2, "then"), t(" : "), i(3, "else")
+--   })
+-- })
+
