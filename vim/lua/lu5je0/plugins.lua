6840363af0eb6a418ac1994bf713edcad1c6f5d4
lu5je0 <lu5je0@gmail.com>
2022-12-03 15:54:18 +0800

    feat: bigfile.nvim

--- vim/lua/lu5je0/plugins.lua
+++ vim/lua/lu5je0/plugins.lua
@@ -724,6 +724,26 @@ return packer.startup(function(use)
   }
 
   -- use {
+  --   "LunarVim/bigfile.nvim",
+  --   config = function()
+  --     require("bigfile").config {
+  --       filesize = 1, -- size of the file in MiB, the plugin round file sizes to the closest MiB
+  --       pattern = { "*" }, -- autocmd pattern
+  --       features = { -- features to disable
+  --         "indent_blankline",
+  --         "illuminate",
+  --         "lsp",
+  --         "treesitter",
+  --         -- "syntax",
+  --         -- "matchparen",
+  --         -- "vimopts",
+  --         -- "filetype",
+  --       },
+  --     }
+  --   end
+  -- }
+
+  -- use {
   --   "samjwill/nvim-unception",
   --   cond = function() return os.getenv('NEOVIM_MEASURE_STARTUP_TIME') ~= 'TRUE' end,
   --   config = function()
