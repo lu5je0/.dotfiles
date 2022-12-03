03a6d8746863a3f2845cc4a6801bfa2ea75425fa
lu5je0 <lu5je0@gmail.com>
2022-12-03 13:27:27 +0800

    feat: dirbuf vim.bo.bufhidden = ""

--- vim/lua/lu5je0/ext/dirbuf.lua
+++ vim/lua/lu5je0/ext/dirbuf.lua
@@ -5,8 +5,10 @@ require("dirbuf").setup {
 vim.api.nvim_create_autocmd('FileType', {
   group = vim.api.nvim_create_augroup('dirbuf', { clear = true }),
   pattern = 'dirbuf',
-  callback = function()
-    vim.bo.bufhidden = ""
+  callback = function(arg)
+    vim.defer_fn(function()
+      vim.bo[arg.buf].bufhidden = ""
+    end, 10)
     -- vim.keymap.set('n', '<leader>e', '<nop>', { buffer = true })
     -- vim.keymap.set('n', '<leader>fe', '<nop>', { buffer = true })
     -- vim.keymap.set('n', '<leader>q', function() vim.cmd('DirbufQuit') end, { buffer = true })
