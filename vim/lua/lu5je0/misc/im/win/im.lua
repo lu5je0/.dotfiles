fe3532f0973ce589307a635b7b4b0d1428228af2
lu5je0 <lu5je0@gmail.com>
2022-12-04 13:44:38 +0800

    feat: update win/im.lua

--- vim/lua/lu5je0/misc/im/win/im.lua
+++ vim/lua/lu5je0/misc/im/win/im.lua
@@ -1,14 +1,32 @@
 local M = {}
 
 local group = vim.api.nvim_create_augroup('ime-status', { clear = true })
-local path = vim.fn.stdpath('config')
+-- local path = vim.fn.stdpath('config')
 
 M.disable_ime = function()
-  io.popen(path .. '/lib/toDisableIME.exe 2>/dev/null'):close()
+  vim.loop.new_thread(function()
+    io.popen('~/.dotfiles/vim/lib/toDisableIME.exe 2>/dev/null'):close()
+  end)
 end
 
 M.enable_ime = function()
-  io.popen(path .. '/lib/toEnableIME.exe 2>/dev/null'):close()
+  if M.save_last_ime then
+    vim.loop.new_thread(function()
+      io.popen('~/.dotfiles/vim/lib/toEnableIME.exe 2>/dev/null'):close()
+    end)
+  end
+end
+
+M.toggle_save_last_ime = function()
+  local keeper = require('lu5je0.misc.env-keeper')
+  local v = keeper.get('save_last_ime', true)
+  if v then
+    print("keep last ime disabled")
+  else
+    print("keep last ime enabled")
+  end
+  M.save_last_ime = not v
+  keeper.set('save_last_ime', M.save_last_ime)
 end
 
 local function create_autocmd()
@@ -30,7 +48,9 @@ local function create_autocmd()
 end
 
 M.setup = function()
+  M.save_last_ime = require('lu5je0.misc.env-keeper').get('save_last_ime', true)
   create_autocmd()
+  vim.keymap.set('n', '<leader>vi', M.toggle_save_last_ime)
 end
 
 return M
