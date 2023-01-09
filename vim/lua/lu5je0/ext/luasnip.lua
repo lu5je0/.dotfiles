021cbf487635e0b5abf19e1f309dd8d9d3c265df
lu5je0 <lu5je0@gmail.com>
2023-01-10 00:14:10 +0800

    fix: luasnip.lua

--- vim/lua/lu5je0/ext/luasnip.lua
+++ vim/lua/lu5je0/ext/luasnip.lua
@@ -2,6 +2,17 @@ local M = {}
 
 local luasnip = require('luasnip')
 
+local cmp = require('cmp')
+local keys = require('lu5je0.core.keys')
+
+-- 修复按下<c-j>后cmp补全位置不对的问题
+local function cmp_hotfix()
+  if cmp.visible() then
+    cmp.close()
+    keys.feedkey('<esc>a')
+  end
+end
+
 M.jump_next_able = function()
   local modified_line = vim.fn.line("'^")
   local current_line = vim.fn.line(".")
@@ -11,13 +22,23 @@ end
 local function keymap()
   local opts = { silent = true }
 
-  vim.keymap.set({ 's', 'i' }, '<c-j>', function() luasnip.jump(1) end, opts)
-  vim.keymap.set({ 's', 'i' }, '<c-k>', function() luasnip.jump(-1) end, opts)
-  vim.keymap.set({ 's' }, '<cr>', function()
-    if luasnip.jumpable() then
-      luasnip.jump(1)
-    end
+  vim.keymap.set({ 's', 'i' }, '<c-j>', function()
+    luasnip.jump(1)
+    cmp_hotfix()
   end, opts)
+  vim.keymap.set({ 's', 'i' }, '<c-k>', function()
+    luasnip.jump(-1)
+    cmp_hotfix()
+  end, opts)
+
+  for _, lhs in ipairs({ '<cr>', '<tab>' }) do
+    vim.keymap.set({ 's', 'i' }, lhs, function()
+      if luasnip.jumpable() then
+        luasnip.jump(1)
+        cmp_hotfix()
+      end
+    end, opts)
+  end
 
   require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./snippets/" } })
 end
