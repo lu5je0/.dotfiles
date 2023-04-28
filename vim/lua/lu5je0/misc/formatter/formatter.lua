7cdc057e13f5f89c5b932bfc6f3608cd04c3d2d9
lu5je0 <lu5je0@gmail.com>
2023-04-28 18:00:16 +0800

    fix: 1.xmap <leader>cf feat: pyright-config

--- vim/lua/lu5je0/misc/formatter/formatter.lua
+++ vim/lua/lu5je0/misc/formatter/formatter.lua
@@ -32,7 +32,7 @@ local function get_format_priority(filetype)
       end
     end
   end
-  
+
   return { M.FORMAT_TOOL_TYPE.LSP, M.FORMAT_TOOL_TYPE.EXTERNAL }
 end
 
@@ -87,7 +87,7 @@ end
 
 local function external_format(format_type, filetype)
   local external_formatter = get_external_formatter(filetype)
-  
+
   if not external_formatter then
     log('miss format config')
     return
@@ -142,11 +142,9 @@ local function keymapping()
       M.format(M.FORMAT_TYPE.FORMAT)
     end, opts)
 
-    vim.cmd("xmap <leader>cf :lua require('lu5je0.misc.formatter.formatter').format('RANGE_FORMAT')<cr>")
-    -- 会在末尾加上一行 todo
-    -- vim.keymap.set('x', '<leader>cf', function()
-    --   M.format(M.FORMAT_TYPE.RANGE_FORMAT)
-    -- end, opts)
+    vim.keymap.set('x', '<leader>cf', function()
+      M.format(M.FORMAT_TYPE.RANGE_FORMAT)
+    end, opts)
   end, 10)
 end
 
