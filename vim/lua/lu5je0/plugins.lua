cba82ef9e3bac6aacf542da8cfae755ed5a46618
lu5je0 <lu5je0@gmail.com>
2023-06-03 15:39:16 +0800

    feat: nvim-neorg

--- vim/lua/lu5je0/plugins.lua
+++ vim/lua/lu5je0/plugins.lua
@@ -624,4 +624,25 @@ require("lazy").setup({
     end,
     ft = { 'markdown' },
   },
+  {
+    "nvim-neorg/neorg",
+    build = ":Neorg sync-parsers",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    ft = { 'norg' },
+    config = function()
+      require("neorg").setup {
+        load = {
+          ["core.defaults"] = {}, -- Loads default behaviour
+          ["core.concealer"] = {}, -- Adds pretty icons to your documents
+          ["core.dirman"] = { -- Manages Neorg workspaces
+          config = {
+            workspaces = {
+              notes = "~/notes",
+            },
+          },
+        },
+      },
+    }
+    end,
+  },
 }, opts)
