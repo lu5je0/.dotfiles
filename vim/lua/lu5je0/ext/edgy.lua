3ced2bb550dca6502bb9b4b7122912e844ff0293
lu5je0 <lu5je0@gmail.com>
2023-06-07 20:48:43 +0800

    feat: update edgy.nvim

---
+++ vim/lua/lu5je0/ext/edgy.lua
@@ -0,0 +1,63 @@
+local M = {}
+
+M.setup = function()
+  require('edgy').setup {
+      animate = {
+        enabled = false
+        -- cps = 300,
+      },
+      wo = {
+        winbar = false,
+        winfixwidth = false,
+        winfixheight = false,
+        winhighlight = "",
+        spell = false,
+        signcolumn = "no",
+      },
+      bottom = {
+        -- toggleterm / lazyterm at the bottom with a height of 40% of the screen
+        -- {
+        --   ft = "toggleterm",
+        --   size = { height = 0.4 },
+        --   filter = function(buf, win)
+        --     return vim.api.nvim_win_get_config(win).relative == ""
+        --   end,
+        -- },
+        {
+          ft = "help",
+          size = { height = 20 },
+          -- only show help buffers
+          filter = function(buf)
+            return vim.bo[buf].buftype == "help"
+          end,
+        },
+        { ft = "spectre_panel", size = { height = 0.4 } },
+      },
+      left = {
+        {
+          title = "nvimtree",
+          ft = "NvimTree",
+          size = { height = 0.5 },
+        },
+        {
+          ft = "Outline",
+          -- pinned = true,
+          open = "SymbolsOutline",
+        },
+        -- {
+        --   title = "undotree",
+        --   ft = "undotree",
+        --   -- size = { height = 0.5 },
+        -- },
+        -- {
+        --   title = "undotree",
+        --   ft = "diff",
+        --   -- size = { height = 0.5 },
+        -- },
+      },
+      right = {
+      }
+    }
+end
+
+return M
--- vim/lua/lu5je0/plugins.lua
+++ vim/lua/lu5je0/plugins.lua
@@ -726,51 +726,9 @@ require("lazy").setup({
   {
     "folke/edgy.nvim",
     event = "VeryLazy",
-    opts = {
-      animate = {
-        enabled = false
-        -- cps = 300,
-      },
-      wo = {
-        winbar = false,
-        winfixwidth = false,
-        winfixheight = false,
-        winhighlight = "",
-        spell = false,
-        signcolumn = "no",
-      },
-      bottom = {
-        -- toggleterm / lazyterm at the bottom with a height of 40% of the screen
-        {
-          ft = "toggleterm",
-          size = { height = 0.4 },
-          filter = function(buf, win)
-            return vim.api.nvim_win_get_config(win).relative == ""
-          end,
-        },
-        {
-          ft = "help",
-          size = { height = 20 },
-          -- only show help buffers
-          filter = function(buf)
-            return vim.bo[buf].buftype == "help"
-          end,
-        },
-        { ft = "spectre_panel", size = { height = 0.4 } },
-      },
-      left = {
-        {
-          title = "nvimtree",
-          ft = "NvimTree",
-          size = { height = 0.5 },
-        },
-        {
-          ft = "Outline",
-          -- pinned = true,
-          open = "SymbolsOutline",
-        },
-      },
-    },
+    config = function()
+      require("lu5je0.ext.edgy").setup()
+    end
   }
   
 }, opts)
