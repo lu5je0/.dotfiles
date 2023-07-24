d8fe61bde4b068137d8aaa24751f906826d380d2
lu5je0 <lu5je0@gmail.com>
2023-07-24 19:52:51 +0800

    update repeat.lua

--- vim/lua/lu5je0/ext/repeat.lua
+++ vim/lua/lu5je0/ext/repeat.lua
@@ -2,33 +2,49 @@ local M = {}
 
 local keys = require('lu5je0.core.keys')
 
-M.repeat_rhs = nil
-
-local function do_repeat_rhs(rhs)
-  keys.feedkey(rhs, 'n')
-  M.repeat_rhs = rhs
-  vim.go.operatorfunc = "v:lua.__repeat_callback"
-  return "g@l"
+-- make a normal mode remap
+local function normal_map(binding, command, opts)
+	opts = opts or { silent = true }
+	vim.keymap.set("n", binding, command, opts)
 end
 
-local function create_repeat_keys(rhs)
-  return function()
-    return do_repeat_rhs(rhs)
+-- make a normal mode remap that is dot-repeatable using vim-repeat plugin
+function M.normal_repeatable_map(binding, rhs)
+  local func
+  if type(rhs) == 'function' then
+    func = rhs
+  else
+    func = function() keys.feedkey(rhs, 'n') end
   end
+  
+  -- map unique Plug mapping using tostring of function
+  local map_name = "<Plug>" .. tostring(func):gsub("function: ", "")
+  -- mapping including vim-repeat magic
+  local repeat_map = map_name .. [[:silent! call repeat#set("\]] .. map_name .. [[", v:count)<CR>]]
+  normal_map(map_name, func)
+  normal_map(binding, repeat_map)
 end
 
-function _G.__repeat_callback()
-  do_repeat_rhs(M.repeat_rhs)
-end
-
-local function register_lhs(lhs)
-  vim.keymap.set("n", lhs, create_repeat_keys(lhs), { expr = true })
+local function register_nmap(lhs)
+    M.normal_repeatable_map(lhs, lhs)
 end
 
 M.setup = function()
-  register_lhs('zfip')
-  register_lhs('zfap')
-  register_lhs('zd')
+  vim.defer_fn(function()
+    register_nmap('<c-w>>')
+    register_nmap('<c-w><')
+    register_nmap('<c-w>+')
+    register_nmap('<c-w>-')
+    
+    register_nmap('zfip')
+    register_nmap('zfap')
+    
+    register_nmap('zfib')
+    register_nmap('zfab')
+    
+    register_nmap('zfiB')
+    register_nmap('zfaB')
+  end, 400)
 end
 
 return M
