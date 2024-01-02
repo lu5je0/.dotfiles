ab59969edacf2a5fcb74f0d9b58b319db3ec1bce
lu5je0 <lu5je0@gmail.com>
2024-01-02 19:21:48 +0800

    feat: nvim-ufo WinNew

--- vim/lua/lu5je0/ext/nvim-ufo.lua
+++ vim/lua/lu5je0/ext/nvim-ufo.lua
@@ -102,6 +102,16 @@ vim.keymap.set('n', 'zo', function()
   trigger_fold_opened()
 end, { noremap = true })
 
+local group = vim.api.nvim_create_augroup('nvim-ufo-patch', { clear = true })
+vim.api.nvim_create_autocmd('WinNew', {
+  group = group,
+  pattern = '*',
+  callback = function()
+    vim.cmd('UfoDetach')
+    vim.cmd('UfoAttach')
+  end,
+})
+
 require('ufo').setup({
   provider_selector = function(bufnr, filetype, buftype)
     if parsers.get_parser(bufnr) then
