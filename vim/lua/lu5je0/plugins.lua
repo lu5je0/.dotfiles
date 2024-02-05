17879732b67200a690c0ccd7825aaa9c449ae2a7
lu5je0 <lu5je0@gmail.com>
2024-02-05 18:58:49 +0800

    feat: bigfile.nvim

--- vim/lua/lu5je0/plugins.lua
+++ vim/lua/lu5je0/plugins.lua
@@ -916,24 +916,24 @@ require("lazy").setup({
     },
   },
   
-  -- {
-  --   "LunarVim/bigfile.nvim",
-  --   config = function()
-  --     require("bigfile").setup {
-  --       filesize = 3, -- size of the file in MiB, the plugin round file sizes to the closest MiB
-  --       pattern = { "*" }, -- autocmd pattern or function see <### Overriding the detection of big files>
-  --       features = { -- features to disable
-  --         "indent_blankline",
-  --         "illuminate",
-  --         "lsp",
-  --         "treesitter",
-  --         "syntax",
-  --         "matchparen",
-  --         "vimopts",
-  --         "filetype",
-  --       },
-  --     }
-  --   end
-  -- }
+  {
+    "LunarVim/bigfile.nvim",
+    config = function()
+      require("bigfile").setup {
+        filesize = 5, -- size of the file in MiB, the plugin round file sizes to the closest MiB
+        pattern = { "*" }, -- autocmd pattern or function see <### Overriding the detection of big files>
+        features = { -- features to disable
+          "indent_blankline",
+          "illuminate",
+          "lsp",
+          "treesitter",
+          "syntax",
+          "matchparen",
+          -- "vimopts",
+          "filetype",
+        },
+      }
+    end
+  }
   
 }, opts)
