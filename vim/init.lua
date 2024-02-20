d542253ca0b0ab78a13e40157ae0394f62518cd4
lu5je0 <lu5je0@gmail.com>
2024-02-20 16:06:26 +0800

    update

--- vim/init.lua
+++ vim/init.lua
@@ -1,8 +1,3 @@
--- font: mac JetBrainsMonoNLNerdFontMono-SemiBold 
--- https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/JetBrainsMono/NoLigatures/SemiBold/JetBrainsMonoNLNerdFontMono-SemiBold.ttf
-
-vim.loader.enable()
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
