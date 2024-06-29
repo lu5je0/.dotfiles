787bf9405fcfebe4fb38c5642a812c1beb1d1d75
lu5je0 <lu5je0@gmail.com>
2024-06-29 14:44:56 +0800

    update

--- vim/lua/lu5je0/ext/gitsigns.lua
+++ vim/lua/lu5je0/ext/gitsigns.lua
@@ -13,28 +13,6 @@ function M.setup()
   require('gitsigns').setup {
     signs = signs,
     signs_staged = signs,
-    -- keymaps = {
-    --   -- Default keymap options
-    --   noremap = true,
-    --
-    --   ['n ]g'] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns.actions\".next_hunk()<CR>'" },
-    --   ['n [g'] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns.actions\".prev_hunk()<CR>'" },
-    --   ['n <leader>ga'] = '<cmd>lua require"gitsigns".stage_buffer()<CR>',
-    --   ['n <leader>gR'] = '<cmd>lua require"gitsigns".reset_buffer_index()<CR>',
-    --   ['n <leader>gt'] = '<cmd>lua require"gitsigns".toggle_deleted()<CR>',
-    --   ['n <leader>gh'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
-    --   ['v <leader>gh'] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-    --   ['n <leader>gH'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-    --   ['n <leader>gu'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
-    --   ['v <leader>gu'] = '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-    --   ['n <leader>gg'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-    --   -- ['n <leader>gB'] = '<cmd>lua require"gitsigns".blame_line(true)<CR>',
-    --   ['n <leader>gw'] = '<cmd>Gitsigns toggle_word_diff<CR>',
-    --
-    --   -- Text objects
-    --   ['o ig'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
-    --   ['x ig'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
-    -- },
     sign_priority = 999,
     on_attach = function(bufnr)
       local gs = package.loaded.gitsigns
