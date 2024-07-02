41d6a50f8651a4f3dbceefddf268a250ff8fbd9d
lu5je0 <lu5je0@gmail.com>
2024-07-02 19:44:53 +0800

    feat: lanuagedetection

---
+++ vim/node/lanuagedetection.mjs
@@ -0,0 +1,36 @@
+import pkg from '@vscode/vscode-languagedetection';
+import * as readline from 'readline'
+
+const { ModelOperations } = pkg;
+
+function readStdin() {
+    return new Promise((resolve, reject) => {
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+            terminal: false
+        });
+
+        let data = '';
+        rl.on('line', (line) => {
+            data += line + '\n';
+        });
+
+        rl.on('close', () => {
+            resolve(data);
+        });
+
+        rl.on('error', (err) => {
+            reject(err);
+        });
+    });
+}
+
+async function main() {
+    let content = await readStdin()
+    const modulOperations = new ModelOperations();
+    const result = await modulOperations.runModel(content);
+    console.log(result[0].languageId);
+}
+
+main()
