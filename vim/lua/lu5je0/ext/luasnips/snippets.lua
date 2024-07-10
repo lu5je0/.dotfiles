bcdf9f0e31ef65c604b0cebe6e409a5ef3bb68f5
lu5je0 <lu5je0@gmail.com>
2024-07-10 19:39:42 +0800

    update snippets.lua

--- vim/lua/lu5je0/ext/luasnips/snippets.lua
+++ vim/lua/lu5je0/ext/luasnips/snippets.lua
@@ -32,7 +32,7 @@ for _, filetype in ipairs(arg_par_filetypes) do
       t(")"),
     })
   })
-  
+
   ls.add_snippets(filetype, {
     postfix({ trig = ".par", match_pattern = "^[\t ]*(.+)$" }, {
       t('('),
@@ -66,32 +66,32 @@ local python_postfix_snippets = (function()
       t(")"), i(1, '')
     })
   })
-  
+
   ls.add_snippets('python', {
     postfix({ trig = '.fori', match_pattern = '^[\t ]*(%d+)$' }, {
       f(function(_, parent)
         return ('for i in range(%s):'):format(parent.snippet.env.POSTFIX_MATCH)
-      end, {}), t({'', ''}),
+      end, {}), t({ '', '' }),
       t('    '), i(''), t(''),
     })
   })
-  
+
   ls.add_snippets('python', {
     postfix({ trig = '.for', match_pattern = '^[\t ]*(.+)$' }, {
       t('for '), i(1, 'item'),
       f(function(_, parent)
         return (' in %s:'):format(parent.snippet.env.POSTFIX_MATCH)
-      end, {}), t({'', ''}),
+      end, {}), t({ '', '' }),
       t('    '), i(2, ''), t(''),
     })
   })
-  
+
   ls.add_snippets('python', {
     postfix({ trig = ".if", match_pattern = '^[\t ]*(.+)$' }, {
       t("if "),
       f(function(_, parent)
         return parent.snippet.env.POSTFIX_MATCH
-      end, {}),t({':', ''}),
+      end, {}), t({ ':', '' }),
       t('    '), i(1, ""),
     })
   })
@@ -150,3 +150,28 @@ end)()
 -- })
 --
 
+-- ls.add_snippets('c', { ts_post({
+--   trig = ".ei",
+--   matchTSNode = {
+--     query = "(if_statement) @prefix",
+--     query_lang = "c",
+--     select = "longest"
+--   },
+--   reparseBuffer = "live"
+-- }, {
+--   d(1, function(_, parent)
+--     if parent.env.LS_TSMATCH == nil then
+--       return sn(nil, t(""))
+--     end
+--     -- tricky: remove indent on lines containing LS_TSMATCH. The
+--     -- indentation is provided by the captured `if`, and should not
+--     -- be prepended again by us.
+--     return sn(nil, {
+--       isn(1, fmt([[
+--   				{} else if ({}) {{]], { t(parent.env.LS_TSMATCH), i(1) }), ""),
+--       t { "", "" },
+--       sn(2, fmt([[
+-- 					{}
+-- 				}}
+-- 			]], { i(1) })) })
+--   end) }) })
