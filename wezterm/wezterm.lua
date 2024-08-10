83241fb9df57b8a2ac69eb687d14a6f43f586ba6
lu5je0 <lu5je0@gmail.com>
2024-08-10 21:08:16 +0800

    feat: wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require('wezterm');
+local mux = wezterm.mux
 
 local uname = (function()
   if string.find(wezterm.target_triple, 'apple') then
@@ -11,7 +12,8 @@ end)()
 local font = (function()
   local r = {}
   if uname == 'win' then
-    r.text_font = wezterm.font("JetBrainsMonoNL Nerd Font Mono", { weight = "Medium", stretch = "Normal", style = "Normal" })
+    r.text_font = wezterm.font("JetBrainsMonoNL Nerd Font Mono",
+      { weight = "Medium", stretch = "Normal", style = "Normal" })
     r.tab_bar_font_size = 10.0
   elseif uname == 'mac' then
     r.text_font = wezterm.font("JetBrainsMonoNL NF", { weight = "Medium", stretch = "Normal", style = "Normal" })
@@ -21,21 +23,30 @@ local font = (function()
 end)()
 
 local config = {
-  initial_cols = 155,
-  initial_rows = 50,
-  default_prog = (function()
+  -- initial_cols = 155,
+  -- initial_rows = 50,
+  default_prog = (function(args)
     if uname == 'win' then
-      return { "wsl" }
+      return { "wsl", "--cd", "~" }
     end
   end)(),
   color_scheme = "Gruvbox Dark (Gogh)",
-  use_resize_increments = true,
+  -- use_resize_increments = true,
   -- ./wezterm.exe ls-fonts --list-system
   font = font.text_font,
   window_frame = {
     font_size = font.tab_bar_font_size,
     active_titlebar_bg = "#2C2E34",
     inactive_titlebar_bg = "#2C2E34",
+  },
+  hide_tab_bar_if_only_one_tab = true,
+  use_fancy_tab_bar = false,
+  -- window_decorations = "RESIZE",
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
   },
   -- window_background_opacity = 0.992,
   -- text_background_opacity = 0.9,
@@ -53,7 +64,7 @@ local config = {
         bg_color = "#2C2E34",
         fg_color = "#808080",
       },
-     new_tab = {
+      new_tab = {
         bg_color = "#2C2E34",
         fg_color = "#808080",
       },
@@ -75,25 +86,28 @@ local config = {
     { key = "j", mods = "LEADER", action = wezterm.action { ActivatePaneDirection = "Down" } },
     { key = "o", mods = "LEADER", action = wezterm.action { ActivatePaneDirection = "Prev" } },
     ---@diagnostic disable-next-line: unused-local
-    { key = "v", mods = "LEADER", action = wezterm.action_callback(function(win, pane)
-      wezterm.log_info(wezterm.target_triple)
-    end) },
+    {
+      key = "v",
+      mods = "LEADER",
+      action = wezterm.action_callback(function(win, pane)
+        wezterm.log_info(wezterm.target_triple)
+      end)
+    },
     -- { key = "o", mods = "LEADER", action = "ActivateLastTab" },
-    { key = "x", mods = "LEADER", action = wezterm.action { CloseCurrentPane = { confirm = true } } },
+    { key = "x",  mods = "LEADER", action = wezterm.action { CloseCurrentPane = { confirm = true } } },
     { key = "\"", mods = "LEADER", action = wezterm.action { SplitVertical = { domain = "CurrentPaneDomain" } } },
-    { key = "c", mods = "LEADER", action = wezterm.action { SpawnTab = "DefaultDomain" } },
-    { key = "n", mods = "LEADER", action = wezterm.action { ActivateTabRelative = 1 } },
-    { key = "t", mods = "ALT", action = wezterm.action { SpawnTab = "DefaultDomain" } },
-  },
-  -- hide_tab_bar_if_only_one_tab = true,
-  -- use_fancy_tab_bar = false,
-  window_decorations = "RESIZE",
-  window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0,
+    { key = "c",  mods = "LEADER", action = wezterm.action { SpawnTab = "DefaultDomain" } },
+    { key = "n",  mods = "LEADER", action = wezterm.action { ActivateTabRelative = 1 } },
+    { key = "t",  mods = "ALT",    action = wezterm.action { SpawnTab = "DefaultDomain" } },
   },
 }
+
+wezterm.on('gui-startup', function(cmd)
+  if cmd then
+    mux.spawn_window { width = 119, height = 45, args = { "wsl", "--cd", cmd.cwd } }
+  else
+    mux.spawn_window { width = 119, height = 45, args = { "wsl", "--cd", "~" } }
+  end
+end)
 
 return config
