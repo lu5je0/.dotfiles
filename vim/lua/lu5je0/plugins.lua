29c158ca734213bd12491d7e36662b668a6f9331
lu5je0 <lu5je0@gmail.com>
2024-08-19 10:32:03 +0800

    feat: CursorHold

--- vim/lua/lu5je0/plugins.lua
+++ vim/lua/lu5je0/plugins.lua
@@ -244,6 +244,7 @@ require("lazy").setup({
       require('lu5je0.ext.nvimtree').setup()
     end,
     cmd = { 'NvimTreeOpen' },
+    event = { 'CursorHold', 'CursorHoldI' },
     keys = { '<leader>e', '<leader>fe' },
   },
   {
