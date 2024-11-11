ea4a66c2ca5737168ad5c1772e4aaa7cf6625bd1
lu5je0 <lu5je0@gmail.com>
2024-11-11 10:37:39 +0800

    feat: win setup.sh

---
+++ win/setup.sh
@@ -0,0 +1,2 @@
+/mnt/c/Windows/System32/cmd.exe /c sudo mklink /d `wslpath -w /mnt/c/Users/lu5je0/.wslconfig` `wslpath -w /mnt/c/Users/lu5je0/.dotfiles/win/wsl2/.wslconfig`
+/mnt/c/Windows/System32/cmd.exe /c sudo mklink /d `wslpath -w /mnt/c/Users/lu5je0/.wezterm.lua` `wslpath -w /mnt/c/Users/lu5je0/.dotfiles/wezterm/wezterm.lua`
