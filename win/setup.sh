af5a016a6f84295646c58a14727cff33ee4b9069
lu5je0 <lu5je0@gmail.com>
2025-03-30 13:46:32 +0800

    wsl

--- win/setup.sh
+++ win/setup.sh
@@ -1,3 +1,19 @@
-/mnt/c/Windows/System32/cmd.exe /c sudo mklink /d `wslpath -w /mnt/c/Users/lu5je0/.wslconfig` `wslpath -w /mnt/c/Users/lu5je0/.dotfiles/win/wsl2/.wslconfig`
-/mnt/c/Windows/System32/cmd.exe /c sudo mklink /d `wslpath -w /mnt/c/Users/lu5je0/.wezterm.lua` `wslpath -w /mnt/c/Users/lu5je0/.dotfiles/wezterm/wezterm.lua`
-/mnt/c/Windows/System32/cmd.exe /c sudo mklink `wslpath -w /mnt/c/Users/lu5je0/.wslconfig` `wslpath -w /mnt/c/Users/lu5je0/.dotfiles/win/wsl2/wslconfig`
+WIN_HOME=/mnt/c/Users/lu5je0
+
+# wezterm
+/mnt/c/Windows/System32/cmd.exe \
+  /c sudo mklink /d \
+  `wslpath -w $WIN_HOME/.wezterm.lua` \
+  `wslpath -w $WIN_HOME/.dotfiles/wezterm/wezterm.lua`
+
+# gitconfig
+/mnt/c/Windows/System32/cmd.exe \
+  /c sudo mklink \
+  `wslpath -w $WIN_HOME/.gitconfig` \
+  `wslpath -w $WIN_HOME/.dotfiles/win/wsl2/gitconfig`
+
+# wslconfig
+/mnt/c/Windows/System32/cmd.exe \
+  /c sudo mklink \
+  `wslpath -w $WIN_HOME/.wslconfig` \
+  `wslpath -w $WIN_HOME/.dotfiles/win/wsl2/wslconfig`
