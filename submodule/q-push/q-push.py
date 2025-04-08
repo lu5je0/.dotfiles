abdfc2ce9c3d722a563875c5ed479e3bc47193d2
lu5je0 <lu5je0@gmail.com>
2025-04-08 17:55:58 +0800

    feat: q-push

---
+++ submodule/q-push/q-push.py
@@ -0,0 +1,37 @@
+import requests
+import argparse
+import os
+
+def push(text: str):
+    headers = {
+        'Content-Type': 'application/json',
+    }
+
+    json_data = {
+        'msg_type': 'text',
+        'content': {
+            'text': text,
+        },
+    }
+
+    token = os.environ['FEISHU_TOKEN']
+    if token == None:
+        print("feishu token is missing")
+        return
+    
+    resp = requests.post(
+        'https://open.feishu.cn/open-apis/bot/v2/hook/' + token,
+        headers=headers,
+        json=json_data,
+    )
+    if resp.status_code != 200:
+        print('push failed', resp)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('msgs', nargs='+')
+    # parser.add_argument("-i", "--img")
+    
+    args = parser.parse_args()
+    
+    push("\n".join(args.msgs))
