13c8c525cb65236393cb4d919a7fe5283f18e3c0
lu5je0 <lu5je0@gmail.com>
2025-04-11 15:25:47 +0800

    remove speedtest

--- zsh/platform.sh
+++ zsh/platform.sh
@@ -26,7 +26,7 @@ if [[ $UNAME_INFO =~ "Darwin" ]]; then
   # echo -en "\033]6;1;bg;green;brightness;46\a"
   # echo -en "\033]6;1;bg;blue;brightness;51\a"
   
-  export PATH=/home/lu5je0/.dotfiles/bin/mac_arm64/:$PATH
+  export PATH=$HOME/.dotfiles/bin/macos_arm64:$PATH
 elif [[ $UNAME_INFO =~ "WSL" ]]; then
   
   # windows 目录使用windows的git
