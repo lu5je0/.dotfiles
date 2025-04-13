ae31bb18dc43fd42a3338bd927f9b1b490895b33
lu5je0 <lu5je0@gmail.com>
2025-04-13 17:16:52 +0800

    update config

--- vim/lsp/pylsp.lua
+++ vim/lsp/pylsp.lua
@@ -42,14 +42,4 @@ return {
       },
     },
   },
-  cmd = { 'pylsp' },
-  filetypes = { 'python' },
-  root_markers = {
-    'pyproject.toml',
-    'setup.py',
-    'setup.cfg',
-    'requirements.txt',
-    'Pipfile',
-    '.git',
-  },
 }
