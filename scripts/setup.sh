861d88bb5b985386dc01f00a4d8e12c0533f0273
lu5je0 <lu5je0@gmail.com>
2025-04-30 22:38:08 +0800

    remove alacritty

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 ask() {
-    echo -n $1
-    echo -n $' (y/n) \n# '
-    read choice
-    case $choice in
-        Y | y)
-        return 0
-    esac
-    return -1
+  echo -n $1
+  echo -n $' (y/n) \n# '
+  read choice
+  case $choice in
+    Y | y)
+      return 0
+  esac
+  return -1
 }
 
 ask "Enable http proxy(http://127.0.0.1:1080)?" && export http_proxy=http://${HTTP_PROXY:-127.0.0.1:1080} && export https_proxy=http://${HTTP_PROXY:-127.0.0.1:1080}
@@ -18,18 +18,18 @@ if [[ ! -d ~/.config ]]; then
 fi
 
 if [ "$(uname)" = "Linux" ]; then
-    if [ -f /etc/lsb-release ]; then
-        # ask "Add add-apt-repository?" && sh ~/.dotfiles/scripts/apt-ppa.sh
-        ask "Install requires(apt)?" && sh ~/.dotfiles/scripts/apt-requirements.sh
-        # ask "Update nodejs?" && curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash - && sudo apt install -y nodejs
-    fi
-    ask "Config pip3 ali index-url?" && sh ~/.dotfiles/scripts/pip3-ali.sh
+  if [ -f /etc/lsb-release ]; then
+    # ask "Add add-apt-repository?" && sh ~/.dotfiles/scripts/apt-ppa.sh
+    ask "Install requires(apt)?" && sh ~/.dotfiles/scripts/apt-requirements.sh
+    # ask "Update nodejs?" && curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash - && sudo apt install -y nodejs
+  fi
+  ask "Config pip3 ali index-url?" && sh ~/.dotfiles/scripts/pip3-ali.sh
 fi
 
 if [[ -f ~/.ssh/config ]]; then
-    mkdir -p ~/.ssh/config
+  mkdir -p ~/.ssh/config
 else
-    ln -s ~/.dotfiles/ssh/config ~/.ssh/config
+  ln -s ~/.dotfiles/ssh/config ~/.ssh/config
 fi
 
 ask "download stardict?" && sh ~/.dotfiles/scripts/download-stardict.sh
@@ -46,20 +46,20 @@ ln -s ~/.dotfiles/ideavimrc ~/.ideavimrc
 ln -s ~/.dotfiles/zshrc ~/.zshrc
 
 if [[ ! -d ~/.cheat ]]; then
-    ln -s ~/.dotfiles/cheat ~/.cheat
+  ln -s ~/.dotfiles/cheat ~/.cheat
 fi
 
 if [ "$(uname)" = "Darwin" ]; then
-    if [[ -f ~/.mac ]]; then
-        touch ~/.mac
-    fi
+  if [[ -f ~/.mac ]]; then
+    touch ~/.mac
+  fi
 fi
 
 if [[ ! -d ~/.local/bin ]]; then
-    mkdir -p ~/.local/bin
+  mkdir -p ~/.local/bin
 fi
 if [[ ! -f ~/.local/bin/solid ]]; then
-    ln -s ~/.dotfiles/bin ~/.local/bin/solid
+  ln -s ~/.dotfiles/bin ~/.local/bin/solid
 fi
 
 if [[ ! -d ~/.config/pip ]]; then
@@ -89,9 +89,9 @@ if [[ ! -d ~/.config/kitty ]]; then
 fi
 
 # alacritty
-if [[ ! -f ~/.config/alacritty/alacritty.conf ]]; then
-    ln -s ~/.dotfiles/alacritty/mac/alacritty.yml ~/.config/alacritty/alacritty.yml
-fi
+# if [[ ! -d ~/.config/alacritty ]]; then
+#   ln -s ~/.dotfiles/alacritty/mac/alacritty.yml ~/.config/alacritty/alacritty.yml
+# fi
 
 mkdir -p ~/.aria2
 ln -s ~/.dotfiles/aria2/aria2.conf ~/.aria2/aria2.conf
@@ -100,7 +100,7 @@ ln -s ~/.dotfiles/aria2/aria2.conf ~/.aria2/aria2.conf
 mkdir -p ~/.config
 
 if [[ ! -d ~/.config/nvim ]]; then
-    ln -s ~/.dotfiles/vim ~/.config/nvim
+  ln -s ~/.dotfiles/vim ~/.config/nvim
 fi
 
 ask "Install pip3 requirements?" && sh ~/.dotfiles/scripts/pip3-requirements.sh
