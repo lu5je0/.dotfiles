a76f1229c3aeb65c95ab102293d2a7d1b78304ad
lu5je0 <lu5je0@gmail.com>
2025-04-30 10:37:45 +0800

    feat: proxy

--- zsh/proxy.sh
+++ zsh/proxy.sh
@@ -2,17 +2,24 @@ export USER_HTTP_PROXY=${USER_HTTP_PROXY:-'http://127.0.0.1:1080'}
 export USER_SOCKS_PROXY=${USER_SOCKS_PROXY:-'socks5://127.0.0.1:1080'}
 
 proxy() {
-  export http_proxy=$USER_HTTP_PROXY
-  export HTTP_PROXY=$USER_HTTP_PROXY
-  export https_proxy=$USER_HTTP_PROXY
-  export HTTPS_PROXY=$USER_HTTP_PROXY
-}
-
-unproxy() {
-  unset http_proxy
-  unset HTTP_PROXY
-  unset https_proxy
-  unset HTTPS_PROXY
+  # 代理状态变量，默认未启用
+  if [[ -z "$PROXY_ENABLED" ]]; then
+    # 启用代理
+    export http_proxy="$USER_HTTP_PROXY"
+    export HTTP_PROXY="$USER_HTTP_PROXY"
+    export https_proxy="$USER_HTTP_PROXY"
+    export HTTPS_PROXY="$USER_HTTP_PROXY"
+    PROXY_ENABLED=true
+    echo "Proxy enabled: $USER_HTTP_PROXY"
+  else
+    # 取消代理
+    unset http_proxy
+    unset HTTP_PROXY
+    unset https_proxy
+    unset HTTPS_PROXY
+    unset PROXY_ENABLED
+    echo "Proxy disabled"
+  fi
 }
 
 git_ssh_proxy() {
