475ca6013763983c509b54eb9349eb2a11f131eb
lu5je0 <lu5je0@gmail.com>
2025-05-06 10:59:38 +0800

    feat: copilot

--- vim/lua/lu5je0/plugins.lua
+++ vim/lua/lu5je0/plugins.lua
@@ -1051,6 +1051,19 @@ local plugins = {
   --     vim.keymap.set('i', '<right>', function()
   --       require("copilot.suggestion").accept()
   --     end)
+  --     vim.api.nvim_create_autocmd("User", {
+  --       pattern = "BlinkCmpMenuOpen",
+  --       callback = function()
+  --         vim.b.copilot_suggestion_hidden = true
+  --       end,
+  --     })
+  --
+  --     vim.api.nvim_create_autocmd("User", {
+  --       pattern = "BlinkCmpMenuClose",
+  --       callback = function()
+  --         vim.b.copilot_suggestion_hidden = false
+  --       end,
+  --     })
   --   end
   -- },
   
