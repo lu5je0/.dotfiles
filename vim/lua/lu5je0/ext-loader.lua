84fa0002cd203bbafc8d8c7975758e9c4ff97171
lu5je0 <lu5je0@gmail.com>
2025-05-10 10:34:18 +0800

    feat: revmove unicode

--- vim/lua/lu5je0/ext-loader.lua
+++ vim/lua/lu5je0/ext-loader.lua
@@ -287,14 +287,6 @@ lazy_load({
   cmd = { 'Base64Decode', 'Base64Encode' }
 })
 
--- unicode
-lazy_load({
-  config = function()
-    require('lu5je0.misc.unicode').create_command()
-  end,
-  cmd = { 'UnicodeEscape', 'UnicodeUnescape' }
-})
-
 -- timestamp
 lazy_load({
   config = function()
