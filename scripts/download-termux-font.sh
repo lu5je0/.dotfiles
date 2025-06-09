d0b3a5576ee85182c111a992c5e901a5de9efd1d
lu5je0 <lu5je0@gmail.com>
2025-06-09 17:14:01 +0800

    feat: download-termux-font

---
+++ scripts/download-termux-font.sh
@@ -0,0 +1,3 @@
+mkdir -p ~/.termux
+cd ~/.termux
+curl -L -o font.ttf https://github.com/ryanoasis/nerd-fonts/raw/refs/heads/master/patched-fonts/JetBrainsMono/NoLigatures/Regular/JetBrainsMonoNLNerdFontMono-Regular.ttf
