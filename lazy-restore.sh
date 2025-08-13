4f10679a301be1dcf5afc4ef638466d80542ac9b
lu5je0 <lu5je0@gmail.com>
2025-08-13 14:28:25 +0800

    fix: lazy-restore

--- lazy-restore.sh
+++ lazy-restore.sh
@@ -1,2 +1,2 @@
 git checkout -- ~/.dotfiles/vim/lazy-lock.json
-nvim --headless +":lua vim.cmd('LazyRestore') require('lazy').load({ plugins = { 'nvim-treesitter' }, opt = { force = true } }); vim.cmd('TSUpdateSync all')" +qa
+nvim --headless +":lua vim.cmd('LazyRestore'); require('lazy').load({ plugins = { 'nvim-treesitter' }, opt = { force = true } }); require('nvim-treesitter.install').update({}, { summary = true }):wait(300000)" +qa
