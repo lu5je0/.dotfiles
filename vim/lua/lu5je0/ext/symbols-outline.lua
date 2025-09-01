c458ec949cf91ac40aa2df5e389e89f1d4d1f04d
lu5je0 <lu5je0@gmail.com>
2025-09-01 17:02:43 +0800

    feat: save outline width

--- vim/lua/lu5je0/ext/symbols-outline.lua
+++ vim/lua/lu5je0/ext/symbols-outline.lua
@@ -1,7 +1,26 @@
 local M = {}
 
+local function get_outline_width()
+  local tab = vim.api.nvim_get_current_tabpage()
+  local wins = vim.api.nvim_tabpage_list_wins(tab)
+  for _, win in ipairs(wins) do
+    local buf = vim.api.nvim_win_get_buf(win)
+    if vim.bo[buf].filetype == "Outline" then
+      return vim.api.nvim_win_get_width(win)
+    end
+  end
+  return nil
+end
+
 local function keymap()
   vim.keymap.set('n', '<leader>i', function()
+    if require('outline').is_open() then
+      local width = get_outline_width()
+      if width then
+        require('outline.config').o.outline_window.width = width
+      end
+    end
+    
     vim.cmd('Outline!')
   end)
   
