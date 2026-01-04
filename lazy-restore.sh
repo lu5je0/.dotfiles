609aa471c7f08caca6a20c52c5ccc453f95f8a1a
lu5je0 <lu5je0@gmail.com>
2026-01-04 11:19:36 +0800

    fix: lazy-restore.sh

--- lazy-restore.sh
+++ lazy-restore.sh
@@ -1,2 +1,16 @@
+#!/bin/bash
+
+LAZY_DIR="$HOME/.local/share/nvim/lazy"
+PLUGIN_LIST=(
+    "LuaSnip"
+)
+
+for PLUGIN in "${PLUGIN_LIST[@]}"; do
+    TARGET="${LAZY_DIR}/${PLUGIN}"
+    if [ -d "$TARGET" ]; then
+      rm $TARGET -rf
+    fi
+done
+
 git checkout -- ~/.dotfiles/vim/lazy-lock.json
 nvim --headless +":lua vim.cmd('LazyRestore'); require('lazy').load({ plugins = { 'nvim-treesitter' }, opt = { force = true } }); require('nvim-treesitter.install').update({}, { summary = true }):wait(300000)" +qa
