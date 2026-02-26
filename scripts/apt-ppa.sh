fe4aae08e8e253cb2153fdd2783556f186657984
lu5je0 <lu5je0@gmail.com>
2026-02-26 15:42:46 +0800

    chore: update setup modules and remove legacy scripts

--- scripts/apt-ppa.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo apt install software-properties-common
-sudo add-apt-repository -y ppa:neovim-ppa/unstable
-sudo apt update
--- scripts/setup.d/modules/40-termux.sh
+++ scripts/setup.d/modules/40-termux.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
-# DESC: link Termux config (~/.config/termux)?
+# DESC: termux config (~/.config/termux)?
 
 DOTFILES_DIR="${DOTFILES_DIR:-$HOME/.dotfiles}"
 TARGET="$HOME/.config/termux"
 
 if [[ -e "$TARGET" ]]; then
   echo "skip: $TARGET exists"
-  exit 0
+else
+  mkdir -p "$HOME/.config"
+  ln -s "$DOTFILES_DIR/termux" "$TARGET"
 fi
 
-mkdir -p "$HOME/.config"
-ln -s "$DOTFILES_DIR/termux" "$TARGET"
+TERMUX_FONT_URL="https://github.com/ryanoasis/nerd-fonts/raw/refs/heads/master/patched-fonts/JetBrainsMono/NoLigatures/Regular/JetBrainsMonoNLNerdFontMono-Regular.ttf"
+TERMUX_FONT_PATH="$HOME/.termux/font.ttf"
+
+mkdir -p "$TERMUX_HOME"
+if [[ -e "$TERMUX_FONT_PATH" ]]; then
+  echo "skip: $TERMUX_FONT_PATH exists"
+else
+  curl -L -o "$TERMUX_FONT_PATH" "$TERMUX_FONT_URL"
+fi
