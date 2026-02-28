86839ef8584aebbc9fab3bae3c1468516de8edbb
lu5je0 <lu5je0@gmail.com>
2026-02-28 22:34:08 +0800

    feat: disable python3 provider

--- vim/lua/lu5je0/options.lua
+++ vim/lua/lu5je0/options.lua
@@ -134,7 +134,7 @@ o.shortmess = o.shortmess .. 'I'
 o.showcmd = false
 
 -- disable some default providers
--- g.loaded_python3_provider = 0
+g.loaded_python3_provider = 0
 g.loaded_node_provider = 0
 g.loaded_perl_provider = 0
 g.loaded_ruby_provider = 0
