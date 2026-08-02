ef3e971077d0a4dec30b07c7e300f6acd2c11cf6
lu5je0 <lu5je0@gmail.com>
2026-08-02 16:48:02 +0800

    trash

--- scripts/pip3-ali.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo pip3 config set global.index-url https://mirrors.aliyun.com/pypi/simple/
--- scripts/pip3-requirements.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-pip3 install pynvim neovim-remote autopep8 croniter asstosrt
