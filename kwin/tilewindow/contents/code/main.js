bc6977da9873e3df039c02876e391dfb48692e8b
lu5je0 <lu5je0@gmail.com>
2026-08-09 15:16:11 +0800

    kwin

--- kwin/tilewindow/contents/code/main.js
+++ kwin/tilewindow/contents/code/main.js
@@ -14,13 +14,13 @@ const layoutConfig = {
     },
     "kitty": {
         "center_i": function(sw, sh) {
-            const w = 1560;
-            const h = 1197;
+            const w = 1562;
+            const h = 1198;
             return { width: w, height: h, x: Math.round((sw - w) / 2), y: Math.round((sh - h) / 2) };
         },
         "center_j": function(sw, sh) {
-            const w = 1140;
-            const h = 973;
+            const w = 1142;
+            const h = 974;
             return { width: w, height: h, x: Math.round((sw - w) / 2), y: Math.round((sh - h) / 2) };
         },
     },
