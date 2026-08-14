f46b5a40828928cdd5e20005e2f4bfb41f1965ca
lu5je0 <lu5je0@gmail.com>
2026-08-14 10:49:48 +0800

    fix: cron.sh

--- bin/common/cron.sh
+++ bin/common/cron.sh
@@ -4,6 +4,9 @@ if [ "$1" = "-r" ] ; then
     echo "My God! Dangerous..."
     echo "Exit..."
     exit 2
+elif [ -z "$1" ] ; then
+    echo "Usage: cron.sh <crontab-file>" >&2
+    exit 1
 else
     crontab "$1"
 fi
